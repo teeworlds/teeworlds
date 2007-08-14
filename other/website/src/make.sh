f1f9d455f271d216fb73ecee6181b49030078777
Magnus Auvinen <magnus.auvinen@gmail.com>
2007-08-14 21:59:16 +0000

    moved /trunk/other to /assets

--- other/website/src/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python make_page.py < content_home.html > ../index.html
-python make_page.py < content_team.html > ../team.html
-python make_page.py < content_download.html > ../download.html
-python make_page.py < content_support.html > ../support.html
--- other/website/src/make_page.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-template = "".join(file("template.html").readlines())
-content = "".join(sys.stdin.readlines())
-final_page = template.replace("%CONTENT%", content)
-print final_page
