66bd7b81d93f9bf3d56cb935ce2e3998e822e1e9
Alfred Eriksson <somerunce@gmail.com>
2008-11-11 17:22:22 +0000

    fixed server side demo recording filename

--- src/engine/server/es_server.c
+++ src/engine/server/es_server.c
@@ -1227,6 +1227,8 @@ static void con_shutdown(void *result, void *user_data)
 
 static void con_record(void *result, void *user_data)
 {
+	char filename[512];
+	str_format(filename, sizeof(filename), "demos/%s.demo", console_arg_string(result, 0));
 	demorec_record_start(console_arg_string(result, 0), mods_net_version(), current_map, current_map_crc, "server");
 }
 
