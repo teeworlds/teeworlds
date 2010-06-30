b0b181f910f00154646df1116ea0b3cf3038b19f
SushiTee <weichel.sascha@gmail.com>
2010-07-01 00:57:37 +0200

    fixed a cheat with restarting race and rocket launcher

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -658,7 +658,8 @@ void CCharacter::Tick()
 			m_Armor++;
 	}
 	
-	if((GameServer()->Collision()->GetIndex(m_Pos.x, m_Pos.y) == TILE_BEGIN || (GameServer()->m_pController->IsFastCap() && ((CGameControllerFC*)GameServer()->m_pController)->IsEnemyFlagStand(m_Pos, m_pPlayer->GetTeam()))) && (!m_aWeapons[WEAPON_GRENADE].m_Got || m_RaceState != RACE_STARTED))
+	if((GameServer()->Collision()->GetIndex(m_Pos.x, m_Pos.y) == TILE_BEGIN && (!m_aWeapons[WEAPON_GRENADE].m_Got || (m_RaceState != RACE_FINISHED && m_RaceState != RACE_STARTED)))
+		|| (GameServer()->m_pController->IsFastCap() && ((CGameControllerFC*)GameServer()->m_pController)->IsEnemyFlagStand(m_Pos, m_pPlayer->GetTeam())))
 	{
 		// create flag
 		if(GameServer()->m_pController->IsFastCap())
