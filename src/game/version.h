29e5ee445c34e1458c3073f3971f563c221a967b
SushiTee <weichel.sascha@gmail.com>
2010-07-12 13:54:32 +0200

    fixed version

--- src/game/version.h
+++ src/game/version.h
@@ -1,7 +1,7 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.c"
-#define GAME_VERSION "trunk"
+#define GAME_VERSION "0.5 trunk"
 #define GAME_NETVERSION "0.5 b67d1f1a1eea234e"
 #define RACE_VERSION "3.0-dev"
 #endif
