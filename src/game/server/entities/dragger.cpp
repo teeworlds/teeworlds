dce24aa01db97f3e79482ea080cf1eeaebbaa049
GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>
2010-08-21 19:46:54 +0200

    need Sleep T.T
    Signed-off-by: GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>

--- src/game/server/entities/dragger.cpp
+++ src/game/server/entities/dragger.cpp
@@ -35,9 +35,9 @@ void CDragger::move()
 		target = ents[i];
 		int res=0;
 		if (!nw)
-			res = GameServer()->Collision()->IntersectNoLaserNW(m_Pos, target->m_Pos, 0, 0);
-		else
 			res = GameServer()->Collision()->IntersectNoLaser(m_Pos, target->m_Pos, 0, 0);
+		else
+			res = GameServer()->Collision()->IntersectNoLaserNW(m_Pos, target->m_Pos, 0, 0);
 
 		if (res==0)
 		{
