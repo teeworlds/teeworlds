9499e04fd70d58a786cb770ddfa01113bcad8d0e
GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>
2010-09-06 07:17:53 +0200

    Removed gfx clear from settings file. Closes #15.
    Signed-off-by: GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>

--- src/game/variables.h
+++ src/game/variables.h
@@ -49,7 +49,7 @@ MACRO_CONFIG_INT(UiColorLht, ui_color_lht, 175, 0, 255, CFGFLAG_CLIENT|CFGFLAG_S
 MACRO_CONFIG_INT(UiColorAlpha, ui_color_alpha, 228, 0, 255, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Interface alpha",0)
 
 MACRO_CONFIG_INT(GfxNoclip, gfx_noclip, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Disable clipping",0)
-MACRO_CONFIG_INT(GfxClearFull, gfx_full_clear, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Only show the game tile",0)
+MACRO_CONFIG_INT(GfxClearFull, gfx_full_clear, 0, 0, 1, CFGFLAG_CLIENT, "Only show the game tile",0)
 
 /* race - client */
 MACRO_CONFIG_INT(ClAutoRecord, cl_auto_record, 1, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Save the best demo of each race",0)
