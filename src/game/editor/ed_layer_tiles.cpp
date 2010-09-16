ee87bb16d2a79f833203117fcf10d87d82259043
oy <Tom_Adams@web.de>
2010-09-16 13:13:53 +0200

    do not rotate/flip gamelayer tiles. Closes #155

--- src/game/editor/ed_layer_tiles.cpp
+++ src/game/editor/ed_layer_tiles.cpp
@@ -182,7 +182,7 @@ int CLayerTiles::BrushGrab(CLayerGroup *pBrush, CUIRect Rect)
 		pGrabbed->m_pEditor = m_pEditor;
 		pGrabbed->m_TexId = m_TexId;
 		pGrabbed->m_Image = m_Image;
-		
+		pGrabbed->m_Game = m_Game;
 		pBrush->AddLayer(pGrabbed);
 		
 		// copy the tiles
@@ -260,6 +260,7 @@ void CLayerTiles::BrushFlipX()
 			m_pTiles[y*m_Width+m_Width-1-x] = Tmp;
 		}
 
+	if(!m_Game)
 	for(int y = 0; y < m_Height; y++)
 		for(int x = 0; x < m_Width; x++)
 			m_pTiles[y*m_Width+x].m_Flags ^= m_pTiles[y*m_Width+x].m_Flags&TILEFLAG_ROTATE ? TILEFLAG_HFLIP : TILEFLAG_VFLIP;
@@ -275,6 +276,7 @@ void CLayerTiles::BrushFlipY()
 			m_pTiles[(m_Height-1-y)*m_Width+x] = Tmp;
 		}
 
+	if(!m_Game)
 	for(int y = 0; y < m_Height; y++)
 		for(int x = 0; x < m_Width; x++)
 			m_pTiles[y*m_Width+x].m_Flags ^= m_pTiles[y*m_Width+x].m_Flags&TILEFLAG_ROTATE ? TILEFLAG_VFLIP : TILEFLAG_HFLIP;
@@ -296,9 +298,12 @@ void CLayerTiles::BrushRotate(float Amount)
 			for(int y = m_Height-1; y >= 0; --y, ++pDst)
 			{
 				*pDst = pTempData[y*m_Width+x];
+				if(!m_Game)
+				{
 				if(pDst->m_Flags&TILEFLAG_ROTATE)
 					pDst->m_Flags ^= (TILEFLAG_HFLIP|TILEFLAG_VFLIP);
 				pDst->m_Flags ^= TILEFLAG_ROTATE;
+			}
 			}
 
 		int Temp = m_Width;
