e1b072acb67730dc80be14e4fe0beda9201295b0
GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>
2010-09-22 03:03:14 +0300

    revert dragger
    Signed-off-by: GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>

--- src/game/server/entities/dragger.cpp
+++ src/game/server/entities/dragger.cpp
@@ -12,89 +12,92 @@
 const int LENGTH=700;
 
 CDragger::CDragger(CGameWorld *pGameWorld, vec2 Pos, float Strength, bool NW)
-: CEntity(pGameWorld, NETOBJTYPE_LASER), m_Targets()
+: CEntity(pGameWorld, NETOBJTYPE_LASER)
 {
 	m_Pos = Pos;
 	m_Strength = Strength;
 	m_EvalTick = Server()->Tick();
 	m_NW = NW;
-	
+
 	GameWorld()->InsertEntity(this);
 }
 
 void CDragger::Move()
 {
-	if (m_Targets.empty())
+	if (m_Target)
 		return;
 	CCharacter *Ents[16];
-	int IdInTeam[16]; 
-	int LenInTeam[16];
-	for (int i = 0; i < 16; i++) {
-		IdInTeam[i] = -1;
-		LenInTeam[i] = 0;
-	}
 	int Num = -1;
 	Num =  GameServer()->m_World.FindEntities(m_Pos,LENGTH, (CEntity**)Ents, 16, NETOBJTYPE_CHARACTER);
+	int Id=-1;
+	int MinLen=0;
 	for (int i = 0; i < Num; i++)
 	{
-		CCharacter * Target = Ents[i];
-		int Res = m_NW ? GameServer()->Collision()->IntersectNoLaserNW(m_Pos, Target->m_Pos, 0, 0) :
-			GameServer()->Collision()->IntersectNoLaser(m_Pos, Target->m_Pos, 0, 0);
+		m_Target = Ents[i];
+		int Res=0;
+		if (!m_NW)
+			Res = GameServer()->Collision()->IntersectNoLaser(m_Pos, m_Target->m_Pos, 0, 0);
+		else
+			Res = GameServer()->Collision()->IntersectNoLaserNW(m_Pos, m_Target->m_Pos, 0, 0);
 
 		if (Res==0)
 		{
-			int Len=length(Target->m_Pos - m_Pos);
-			if (LenInTeam[Target->Team()] == 0 || LenInTeam[Target->Team()] > Len)
+			int Len=length(Ents[i]->m_Pos - m_Pos);
+			if (MinLen==0 || MinLen>Len)
 			{
-				LenInTeam[Target->Team()] = Len;
-				IdInTeam[Target->Team()] = i;
+				MinLen=Len;
+				Id=i;
 			}
 		}
 	}
-	for (int i = 0; i < 16; i++) {
-		if(IdInTeam[i] != -1) {
-			CCharacter *Target = Ents[IdInTeam[i]];
-			m_Targets.push_back(Target);
-		}
+	if (Id!=-1)
+	{
+		m_Target = Ents[Id];
+	}
+	else
+	{
+		m_Target=0;
 	}
 }
 
 void CDragger::Drag()
 {
-	if (!m_Targets.empty())
+	if (m_Target)
 	{
-/*
-		int Res = m_NW ? GameServer()->Collision()->IntersectNoLaserNW(m_Pos, m_Target->m_Pos, 0, 0) :
-			GameServer()->Collision()->IntersectNoLaser(m_Pos, m_Target->m_Pos, 0, 0);
-		if (Res || length(m_Pos-m_Target->m_Pos)>700)//TODO: USE CONSTANTS IDIOT
+
+		int Res = 0;
+		if (!m_NW)
+			Res = GameServer()->Collision()->IntersectNoLaser(m_Pos, m_Target->m_Pos, 0, 0);
+		else
+			Res = GameServer()->Collision()->IntersectNoLaserNW(m_Pos, m_Target->m_Pos, 0, 0);
+		if (Res || length(m_Pos-m_Target->m_Pos)>700)
 		{
-			m_Targets.clear();
+			m_Target=0;
 		}
-		else {
-			for(std::list<CCharacter * >::iterator i = m_Targets.begin(); i != m_Targets.end(); ++i) {
-				CCharacter * Target = *i;
-				if (length(m_Pos-Target->m_Pos)>28) {
-					if(((m_Target->m_TileIndexL == TILE_STOPA || m_Target->m_TileFIndexL == TILE_STOPA || m_Target->m_TileIndex == TILE_STOPL || m_Target->m_TileIndexL == TILE_STOPL || m_Target->m_TileFIndex == TILE_STOPL || m_Target->m_TileFIndexL == TILE_STOPL || m_Target->m_TileIndexL == TILE_STOPH || m_Target->m_TileFIndexL == TILE_STOPH)) || ((m_Target->m_TileIndexR == TILE_STOPA || m_Target->m_TileFIndexR == TILE_STOPA || m_Target->m_TileIndex == TILE_STOPR || m_Target->m_TileIndexR == TILE_STOPR || m_Target->m_TileFIndex == TILE_STOPR || m_Target->m_TileFIndexR == TILE_STOPR || m_Target->m_TileIndexR == TILE_STOPH || m_Target->m_TileFIndexR == TILE_STOPH)))
+		else
+			if (length(m_Pos-m_Target->m_Pos)>28)
+			{
+				//vec2 Temp = m_Target->m_Core.m_Vel +(normalize(m_Pos-m_Target->m_Pos)*m_Strength);
+				if(((m_Target->m_TileIndexL == TILE_STOPA || m_Target->m_TileFIndexL == TILE_STOPA || m_Target->m_TileIndex == TILE_STOPL || m_Target->m_TileIndexL == TILE_STOPL || m_Target->m_TileFIndex == TILE_STOPL || m_Target->m_TileFIndexL == TILE_STOPL || m_Target->m_TileIndexL == TILE_STOPH || m_Target->m_TileFIndexL == TILE_STOPH)) || ((m_Target->m_TileIndexR == TILE_STOPA || m_Target->m_TileFIndexR == TILE_STOPA || m_Target->m_TileIndex == TILE_STOPR || m_Target->m_TileIndexR == TILE_STOPR || m_Target->m_TileFIndex == TILE_STOPR || m_Target->m_TileFIndexR == TILE_STOPR || m_Target->m_TileIndexR == TILE_STOPH || m_Target->m_TileFIndexR == TILE_STOPH)))
 					{
 						m_Target->m_Core.m_Vel.y +=(normalize(m_Pos-m_Target->m_Pos)*m_Strength).y;
 						//dbg_msg("","x");
 						return;
 					}
-					if(((m_Target->m_TileIndexB == TILE_STOPA || m_Target->m_TileFIndexB == TILE_STOPA || m_Target->m_TileIndex == TILE_STOPB || m_Target->m_TileIndexB == TILE_STOPB || m_Target->m_TileFIndex == TILE_STOPB || m_Target->m_TileFIndexB == TILE_STOPB|| m_Target->m_TileIndexB == TILE_STOPV || m_Target->m_TileFIndexB == TILE_STOPV)) || ((m_Target->m_TileIndexT == TILE_STOPA || m_Target->m_TileFIndexT == TILE_STOPA || m_Target->m_TileIndex == TILE_STOPT || m_Target->m_TileIndexT == TILE_STOPT || m_Target->m_TileFIndex == TILE_STOPT || m_Target->m_TileFIndexT == TILE_STOPT || m_Target->m_TileIndexT == TILE_STOPV || m_Target->m_TileFIndexT == TILE_STOPV)))
+				if(((m_Target->m_TileIndexB == TILE_STOPA || m_Target->m_TileFIndexB == TILE_STOPA || m_Target->m_TileIndex == TILE_STOPB || m_Target->m_TileIndexB == TILE_STOPB || m_Target->m_TileFIndex == TILE_STOPB || m_Target->m_TileFIndexB == TILE_STOPB|| m_Target->m_TileIndexB == TILE_STOPV || m_Target->m_TileFIndexB == TILE_STOPV)) || ((m_Target->m_TileIndexT == TILE_STOPA || m_Target->m_TileFIndexT == TILE_STOPA || m_Target->m_TileIndex == TILE_STOPT || m_Target->m_TileIndexT == TILE_STOPT || m_Target->m_TileFIndex == TILE_STOPT || m_Target->m_TileFIndexT == TILE_STOPT || m_Target->m_TileIndexT == TILE_STOPV || m_Target->m_TileFIndexT == TILE_STOPV)))
 					{
 						m_Target->m_Core.m_Vel.x +=(normalize(m_Pos-m_Target->m_Pos)*m_Strength).x;
 						//dbg_msg("y","");
 						return;
 					}
 				//m_Target->m_Core.m_Vel = Temp;
-					m_Target->m_Core.m_Vel +=(normalize(m_Pos-m_Target->m_Pos)*m_Strength);
+				m_Target->m_Core.m_Vel +=(normalize(m_Pos-m_Target->m_Pos)*m_Strength);
 			}
 	}
 }
 
 void CDragger::Reset()
 {
-	m_Targets.clear();
 	GameServer()->m_World.DestroyEntity(this);
 }
 
@@ -119,13 +122,10 @@ void CDragger::Tick()
 
 void CDragger::Snap(int SnappingClient)
 {
-	/*
-	if (m_Targets.empty())
+	if (m_Target)
 	{
-		for(std::list<CCharacter * >::iterator i = m_Targets.begin(); i != m_Targets.end(); ++i) {
-			if(NetworkClipped(SnappingClient, m_Pos) && NetworkClipped(SnappingClient,(*i)->m_Pos))
-				return;
-		}
+		if(NetworkClipped(SnappingClient, m_Pos) && NetworkClipped(SnappingClient,m_Target->m_Pos))
+			return;
 	}
 	else
 		if(NetworkClipped(SnappingClient,m_Pos))
@@ -156,7 +156,6 @@ void CDragger::Snap(int SnappingClient)
 	else if (StartTick>Server()->Tick())
 		StartTick = Server()->Tick();
 	obj->m_StartTick = StartTick;
-	*/
 }
 //я тут был
 //я тоже
