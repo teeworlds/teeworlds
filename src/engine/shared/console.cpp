838f9d5bb2c25bb25de94bcbfe5f7e5bc46ed12f
GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>
2010-10-12 10:57:22 +0200

    something was wrong with the if xD now only the admin has too weak connection for this

--- src/engine/shared/console.cpp
+++ src/engine/shared/console.cpp
@@ -618,16 +618,13 @@ void CConsole::List(const int Level, int Flags)
 	while(pCommand)
 	{
 		if(pCommand)
-			if(pCommand->m_Level <= Level && (!Flags)?true:pCommand->m_Flags&Flags)
-			{
-				char aBuf[128];
-				str_format(aBuf,sizeof(aBuf),"Name: %s, Parameters: %s, Help: %s",pCommand->m_pName, (!str_length(pCommand->m_pParams))?"None.":pCommand->m_pParams, (!str_length(pCommand->m_pHelp))?"No Help String Given":pCommand->m_pHelp);
-				PrintResponse(IConsole::OUTPUT_LEVEL_STANDARD, "Console", aBuf);
-				for (int var = 0; var < 500; ++var)
-				{
-					;//do nothing
-				}
-			}
+			if((pCommand->m_Level <= Level))
+					if((!Flags)?true:pCommand->m_Flags&Flags)
+					{
+						char aBuf[128];
+						str_format(aBuf,sizeof(aBuf),"Name: %s, Parameters: %s, Help: %s",pCommand->m_pName, (!str_length(pCommand->m_pParams))?"None.":pCommand->m_pParams, (!str_length(pCommand->m_pHelp))?"No Help String Given":pCommand->m_pHelp);
+						PrintResponse(IConsole::OUTPUT_LEVEL_STANDARD, "Console", aBuf);
+					}
 		pCommand = pCommand->m_pNext;
 	}
 }
