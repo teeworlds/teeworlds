04d3ae8173574ce889f4ac6765fe230a5fd8bf4d
GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>
2010-10-18 10:40:49 +0200

    forgot to add these to make max speed less powerful than normal force

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -877,12 +877,12 @@ void CCharacter::HandleTiles(int Index)
 				)
 				m_Core.m_Pos = m_PrevPos;
 		TempVel += Direction * Force;
-		MaxVel = Direction*(MaxSpeed);
+		MaxVel = Direction*(MaxSpeed/5);
 		if(MaxSpeed && Direction.x && (TempVel.x > MaxVel.x && MaxVel.x > 0 || TempVel.x < MaxVel.x && MaxVel.x < 0))
-			TempVel.x = MaxVel.x;
+			TempVel.x = MaxVel.x + Direction.x;
 		if(MaxSpeed && Direction.y && (TempVel.y > MaxVel.y && MaxVel.y > 0 || TempVel.y < MaxVel.y && MaxVel.y < 0))
 			TempVel.y = MaxVel.y;
-		m_Core.m_Vel = TempVel;
+		m_Core.m_Vel = TempVel + Direction.y;
 		//dbg_msg("speedup tile end","(Direction*Force) %f %f   m_Core.m_Vel%f %f",(Direction*Force).x,(Direction*Force).y,m_Core.m_Vel.x,m_Core.m_Vel.y);
 		//dbg_msg("speedup tile end","Direction %f %f, Force %d, Max Speed %d", (Direction).x,(Direction).y, Force, MaxSpeed);
 	}
@@ -898,7 +898,8 @@ void CCharacter::HandleTiles(int Index)
 				((Direction.y < 0) && ((int)GameServer()->Collision()->GetPos(MapIndexT).y) && ((int)GameServer()->Collision()->GetPos(MapIndexT).y < (int)m_Core.m_Pos.y))
 				)
 				m_Core.m_Pos = m_PrevPos;
-		m_Core.m_Vel = Direction*Force;
+		Force /= 5;
+		m_Core.m_Vel = Direction*Force + Direction;
 		//dbg_msg("Direction","%f %f   %f %f   %f %f",Direction.x,Direction.y,(Direction*Force).x,(Direction*Force).y,m_Core.m_Vel.x,m_Core.m_Vel.y);
 	}
 	m_LastBooster = MapIndex;
