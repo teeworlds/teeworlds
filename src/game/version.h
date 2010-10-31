862c6544c5499c764451e5e25814d987d2ca48f0
GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>
2010-10-31 04:31:36 +0200

    Fixed Server would not shown as compatible version

--- src/game/version.h
+++ src/game/version.h
@@ -1,7 +1,7 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 
-#define DDRACE_VERSION "DDRace 1.0a, 0.5 trunk"
-#define GAME_VERSION "DDRace 1.0a, 0.5 trunk"
+#define DDRACE_VERSION "0.5 trunk, DDRace 1.0a"
+#define GAME_VERSION "0.5 trunk, DDRace 1.0a"
 #define GAME_NETVERSION "0.5 b67d1f1a1eea234e"
 #endif
