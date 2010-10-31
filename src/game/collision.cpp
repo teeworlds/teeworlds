b68e97b35463cd973c87d37fc6e978228bf30c20
GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>
2010-10-31 19:38:35 +0200

    pi

--- src/game/collision.cpp
+++ src/game/collision.cpp
@@ -651,7 +651,7 @@ void CCollision::GetSpeedup(int Index, vec2 *Dir, int *Force, int *MaxSpeed)
 	if(Index < 0)
 		return;
 	vec2 Direction = vec2(1, 0);
-	float Angle = m_pSpeedup[Index].m_Angle * (asin(1.0f) * 2 / 180.0f);
+	float Angle = m_pSpeedup[Index].m_Angle * (pi / 180.0f);
 	*Force = m_pSpeedup[Index].m_Force;
 	*Dir = vec2(cos(Angle), sin(Angle));
 	if(MaxSpeed)
