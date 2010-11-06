ec2387f0e98a42f2511eceb002cab866fd669395
btd <bardadymchik@gmail.com>
2010-11-06 19:54:59 +0300

    Spectator can see all events

--- src/game/server/teams.cpp
+++ src/game/server/teams.cpp
@@ -138,7 +138,8 @@ int CGameTeams::TeamMask(int Team) {
 	if(Team == TEAM_SUPER) return -1;
 	int Mask = 0;
 	for(int i = 0; i < MAX_CLIENTS; ++i) {
-		if(Character(i) && (m_Core.Team(i) == Team || m_Core.Team(i) == TEAM_SUPER)) {
+		if((Character(i) && (m_Core.Team(i) == Team || m_Core.Team(i) == TEAM_SUPER)) 
+			|| (GameServer()->m_apPlayers[i] && GameServer()->m_apPlayers[i]->GetTeam() == -1)) {
 			Mask |= 1 << i;
 		}
 	}
