edc563ac7dfd60baea509ec7ebee1618f3ea1264
Sworddragon <sworddragon2@aol.com>
2010-11-20 18:37:14 +0800

    Updated copyrights

--- src/game/client/components/items.cpp
+++ src/game/client/components/items.cpp
@@ -1,3 +1,5 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include <engine/graphics.h>
 #include <engine/demo.h>
 #include <game/generated/protocol.h>
