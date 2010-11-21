62fcf94b241d3ffffb6802372e34f25f849c13b2
Sworddragon <sworddragon2@aol.com>
2010-11-21 04:37:02 +0100

    Enhanced update_localization.py

--- scripts/update_localization.py
+++ scripts/update_localization.py
@@ -1,4 +1,7 @@
-import sys, os
+import os, re, sys
+match = re.search("(.*?)/[^/]*?$", sys.argv[0])
+if match != None:
+	os.chdir(os.getcwd() + "/" + match.group(1))
 
 source_exts = [".c", ".cpp", ".h"]
 
@@ -20,21 +23,21 @@ def parse_source():
 			if filename[-2:] in source_exts or filename[-4:] in source_exts:
 				for line in open(filename, "rb"):
 					process_line(line)
-	
+
 	return stringtable
 
 def load_languagefile(filename):
 	f = open(filename, "rb")
 	lines = f.readlines()
 	f.close()
-	
+
 	stringtable = {}
 
 	for i in range(0, len(lines)-1):
 		l = lines[i].decode("utf-8").strip()
 		if len(l) and not l[0] == '=' and not l[0] == '#':
 			stringtable[l] = lines[i+1][3:].decode("utf-8").rstrip()
-	
+
 	return stringtable
 
 def generate_languagefile(outputfilename, srctable, loctable):
@@ -55,7 +58,6 @@ def generate_languagefile(outputfilename, srctable, loctable):
 			content += "%s\n== %s\n\n" % (k, loctable[k])
 			num_items += 1
 
-
 	content += "##### needs translation #####\n\n"
 	for k in srctable_keys:
 		if not k in loctable or len(loctable[k]) == 0:
@@ -69,7 +71,7 @@ def generate_languagefile(outputfilename, srctable, loctable):
 			content += "%s\n== %s\n\n" % (k, loctable[k])
 			num_items += 1
 			old_items += 1
-	
+
 	f.write(content.encode("utf-8"))
 	f.close()
 	print("%-40s %8d %8d %8d" % (outputfilename, num_items, new_items, old_items))
@@ -81,8 +83,9 @@ print("%-40s %8s %8s %8s" % ("filename", "total", "new", "old"))
 for filename in os.listdir("../data/languages"):
 	if not ".txt" in filename:
 		continue
-	if filename == "index.txt":
-		continue
-		
+
 	filename = "../data/languages/" + filename
-	generate_languagefile(filename, srctable, load_languagefile(filename))
+	if filename == "../data/languages/index.txt":
+		generate_languagefile(filename, load_languagefile(filename), load_languagefile(filename))
+	else:
+		generate_languagefile(filename, srctable, load_languagefile(filename))
