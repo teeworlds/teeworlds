cdd8184f89bce012847d0afc9ea48c29540f135b
Choupom <andycootlapin@hotmail.fr>
2010-11-25 16:16:52 +0100

    fixed "Kicked by console (Kicked by vote)" drop message

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -1257,7 +1257,7 @@ void CServer::ConKick(IConsole::IResult *pResult, void *pUser)
 	if(pResult->NumArguments() > 1)
 	{
 		char aBuf[128];
-		str_format(aBuf, sizeof(aBuf), "Kicked by console (%s)", pResult->GetString(1));
+		str_format(aBuf, sizeof(aBuf), "Kicked (%s)", pResult->GetString(1));
 		((CServer *)pUser)->Kick(pResult->GetInteger(0), aBuf);
 	}
 	else
