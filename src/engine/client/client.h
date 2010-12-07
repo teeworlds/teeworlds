2910d31ea935b251357a235cb1f79badf001cee4
oy <Tom_Adams@web.de>
2010-12-08 00:42:32 +0100

    added an optional time stamp to a demo record by Batchyx

--- src/engine/client/client.h
+++ src/engine/client/client.h
@@ -293,7 +293,7 @@ public:
 	const char *DemoRecord(const char *pName);
 	virtual void DemoRecord_Stop();
 	const char *DemoPlayer_Play(const char *pFilename, int StorageType);
-	void DemoRecorder_Start(const char *pFilename);
+	void DemoRecorder_Start(const char *pFilename, bool WithTimestamp);
 	void DemoRecorder_Stop();
 
 	virtual class CEngine *Engine() { return &m_Engine; }
