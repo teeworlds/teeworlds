efe36ac955bb290ac963eb6332b0c52d5b3638d8
oy <Tom_Adams@web.de>
2010-12-08 01:42:32 +0100

    added auto recording support by Batchyx

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -1305,6 +1305,7 @@ void CClient::ProcessPacket(CNetChunk *pPacket)
 							m_aSnapshots[SNAP_CURRENT] = m_SnapshotStorage.m_pLast;
 							m_LocalStartTime = time_get();
 							SetState(IClient::STATE_ONLINE);
+							DemoRecorder_HandleAutoStart();
 						}
 
 						// adjust game time
@@ -2007,6 +2008,12 @@ void CClient::DemoRecorder_Start(const char *pFilename, bool WithTimestamp)
 			str_format(aFilename, sizeof(aFilename), "demos/%s.demo", pFilename);
 		m_DemoRecorder.Start(Storage(), m_pConsole, aFilename, GameClient()->NetVersion(), m_aCurrentMap, m_CurrentMapCrc, "client");
 	}
+}
+
+void CClient::DemoRecorder_HandleAutoStart()
+{
+	if(g_Config.m_ClAutoDemoRecord)
+		DemoRecorder_Start("autorecord", true);
 }
 
 void CClient::DemoRecorder_Stop()
