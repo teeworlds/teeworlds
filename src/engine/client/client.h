4756c031c5bfd931009d16ff4bb84be912508422
oy <Tom_Adams@web.de>
2010-12-08 01:55:13 +0100

    save auto recorded demos in a separate folder and added an option to enable/disable it in the settings menu

--- src/engine/client/client.h
+++ src/engine/client/client.h
@@ -284,6 +284,7 @@ public:
 	void RegisterCommands();
 
 	const char *DemoPlayer_Play(const char *pFilename, int StorageType);
+	void DemoRecorder_Init();
 	void DemoRecorder_Start(const char *pFilename, bool WithTimestamp);
 	void DemoRecorder_HandleAutoStart();
 	void DemoRecorder_Stop();
