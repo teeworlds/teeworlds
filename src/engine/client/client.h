629ac24304a5804e22e3f4e75cf1c4e014e19e03
oy <Tom_Adams@web.de>
2010-12-08 08:42:32 +0800

    added auto recording support by Batchyx

--- src/engine/client/client.h
+++ src/engine/client/client.h
@@ -285,6 +285,7 @@ public:
 
 	const char *DemoPlayer_Play(const char *pFilename, int StorageType);
 	void DemoRecorder_Start(const char *pFilename, bool WithTimestamp);
+	void DemoRecorder_HandleAutoStart();
 	void DemoRecorder_Stop();
 
 	virtual class CEngine *Engine() { return &m_Engine; }
