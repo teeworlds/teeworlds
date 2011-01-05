2c91e8e50c41c678e4405598a01c3c4c797a507d
Sworddragon <sworddragon2@aol.com>
2011-01-06 07:51:31 +0800

    Made compiling process Python 3 compatible

--- datasrc/datatypes.py
+++ datasrc/datatypes.py
@@ -1,5 +1,5 @@
+import sys
 
-	
 GlobalIdCounter = 0
 def GetID():
 	global GlobalIdCounter
@@ -33,7 +33,7 @@ class BaseType:
 	def __init__(self, type_name):
 		self._type_name = type_name
 		self._target_name = "INVALID"
-		self._id = GetID() # this is used to remeber what order the members have in structures etc
+		self._id = GetID() # this is used to remember what order the members have in structures etc
 	
 	def Identifyer(self): return "x"+str(self._id)
 	def TargetName(self): return self._target_name
@@ -57,18 +57,18 @@ class Struct(BaseType):
 	def __init__(self, type_name):
 		BaseType.__init__(self, type_name)
 	def Members(self):
-		def sorter(a,b):
-			return a.var.ID()-b.var.ID()
+		def sorter(a):
+			return a.var.ID()
 		m = []
 		for name in self.__dict__:
 			if name[0] == "_":
 				continue
 			m += [MemberType(name, self.__dict__[name])]
 		try:
-			m.sort(sorter)
+			m.sort(key = sorter)
 		except:
 			for v in m:
-				print v.name, v.var
+				print(v.name, v.var)
 			sys.exit(-1)
 		return m
 		
@@ -172,15 +172,15 @@ class Pointer(BaseType):
 
 def EmitTypeDeclaration(root):
 	for l in root().EmitTypeDeclaration(""):
-		print l
+		print(l)
 
 def EmitDefinition(root, name):
 	for l in root.EmitPreDefinition(name):
-		print l
-	print "%s %s = " % (root.TypeName(), name)
+		print(l)
+	print("%s %s = " % (root.TypeName(), name))
 	for l in root.EmitDefinition(name):
-		print l
-	print ";"
+		print(l)
+	print(";")
 
 # Network stuff after this
 
