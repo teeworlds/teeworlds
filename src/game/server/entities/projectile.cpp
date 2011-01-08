df47aedbfebd9d5ab71cb8aeeede83a77c36de95
oy <Tom_Adams@web.de>
2011-01-08 11:34:19 +0100

    reset flags and projectiles when leaving the game layer too much. Closes #385

--- src/game/server/entities/projectile.cpp
+++ src/game/server/entities/projectile.cpp
@@ -67,7 +67,7 @@ void CProjectile::Tick()
 
 	m_LifeSpan--;
 	
-	if(TargetChr || Collide || m_LifeSpan < 0)
+	if(TargetChr || Collide || m_LifeSpan < 0 || GameLayerClipped(CurPos))
 	{
 		if(m_LifeSpan >= 0 || m_Weapon == WEAPON_GRENADE)
 			GameServer()->CreateSound(CurPos, m_SoundImpact);
