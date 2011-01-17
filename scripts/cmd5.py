ed46328dabcfd61e996b13427d41b110bf9311a1
oy <Tom_Adams@web.de>
2011-01-17 22:48:57 +0800

    added Python 3 support fixes by Sworddragon

--- scripts/cmd5.py
+++ scripts/cmd5.py
@@ -3,15 +3,15 @@ import hashlib, sys, re
 alphanum = "0123456789abcdefghijklmnopqrstuvwzyxABCDEFGHIJKLMNOPQRSTUVWXYZ_"
 
 def cstrip(lines):
-	d = ""	
+	d = b""	
 	for l in lines:
-		l = re.sub("#.*", "", l)
-		l = re.sub("//.*", "", l)
-		d += l + " "
-	d = re.sub("\/\*.*?\*/", "", d) # remove /* */ comments
-	d = d.replace("\t", " ") # tab to space
-	d = re.sub("  *", " ", d) # remove double spaces
-	d = re.sub("", "", d) # remove /* */ comments
+		l = re.sub(b"#.*", b"", l)
+		l = re.sub(b"//.*", b"", l)
+		d += l + b" "
+	d = re.sub(b"\/\*.*?\*/", b"", d) # remove /* */ comments
+	d = d.replace(b"\t", b" ") # tab to space
+	d = re.sub(b"  *", b" ", d) # remove double spaces
+	d = re.sub(b"", b"", d) # remove /* */ comments
 	
 	d = d.strip()
 	
@@ -24,11 +24,11 @@ def cstrip(lines):
 		i += 1
 	return d
 
-f = ""
+f = b""
 for filename in sys.argv[1:]:
 	f += cstrip([l.strip() for l in open(filename, "rb")])
 
-hash = hashlib.md5(f.encode()).hexdigest().lower()[16:]
+hash = hashlib.md5(f).hexdigest().lower()[16:]
 # TODO: refactor hash that is equal to the 0.5 hash, remove when we 
 # TODO: remove when we don't need it any more
 if hash == "026b8eceb4cdd369":
