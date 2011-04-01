4fa3825e5ca83aeea394b644ff41fde0c1723fc7
oy <Tom_Adams@web.de>
2011-04-01 17:25:02 +0200

    fixed compiling of tileset_borderfix

--- src/tools/tileset_borderfix.cpp
+++ src/tools/tileset_borderfix.cpp
@@ -1,5 +1,6 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
+#include <stdlib.h>
 #include <base/system.h>
 #include <engine/external/pnglite/pnglite.h>
 
