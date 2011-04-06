95ce20b94acb70dcc6d24961988789382fc4fac7
oy <Tom_Adams@web.de>
2011-04-06 11:41:40 +0200

    improved call vote tab. Closes #573

--- src/game/client/components/menus.cpp
+++ src/game/client/components/menus.cpp
@@ -125,17 +125,6 @@ int CMenus::DoButton_MenuTab(const void *pID, const char *pText, int Checked, co
 	return UI()->DoButtonLogic(pID, pText, Checked, pRect);
 }
 
-int CMenus::DoButton_PageMenu(const void *pID, const char *pText, int Checked, bool Active, const CUIRect *pRect, int Corners)
-{
-	if(Active)
-		RenderTools()->DrawUIRect(pRect, vec4(1,1,1,0.5f)*ButtonColorMul(pID), Corners, 10.0f);
-	else
-		RenderTools()->DrawUIRect(pRect, vec4(0.0f, 0.0f, 0.0f, 0.25f), Corners, 10.0f);
-	UI()->DoLabel(pRect, pText, pRect->h*ms_FontmodHeight, 0);
-	
-	return UI()->DoButtonLogic(pID, pText, Checked, pRect);
-}
-
 int CMenus::DoButton_ListRow(const void *pID, const char *pText, int Checked, const CUIRect *pRect)
 {
 	if(Checked)
