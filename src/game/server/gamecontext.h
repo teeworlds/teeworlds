dce5222f7854ee35ce056c24ab48fd78d268426b
SushiTee <weichel.sascha@xxx.xx>
2011-04-07 19:45:05 +0200

    removed unused function

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -114,7 +114,6 @@ public:
 	// helper functions
 	void CreateDamageInd(vec2 Pos, float AngleMod, int Amount);
 	void CreateExplosion(vec2 Pos, int Owner, int Weapon, bool NoDamage);
-	void CreateSmoke(vec2 Pos);
 	void CreateHammerHit(vec2 Pos);
 	void CreatePlayerSpawn(vec2 Pos);
 	void CreateDeath(vec2 Pos, int Who);
