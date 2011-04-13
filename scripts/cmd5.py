43a6c022638278a190c450482bf4eb04cfaafb53
oy <Tom_Adams@web.de>
2011-04-13 20:48:50 +0200

    fixed the nethash

--- scripts/cmd5.py
+++ scripts/cmd5.py
@@ -30,6 +30,6 @@ for filename in sys.argv[1:]:
 
 hash = hashlib.md5(f).hexdigest().lower()[16:]
 #TODO 0.7: improve nethash creation
-if hash == "e20714704d311d78":
+if hash == "6e28a475de43adfd":
 	hash = "626fce9a778df4d4"
 print('#define GAME_NETVERSION_HASH "%s"' % hash)
