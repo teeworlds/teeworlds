4ae9823393364190b33450fc9cd4059330de76e9
oy <Tom_Adams@web.de>
2011-06-15 01:03:14 +0200

    fixed spawn bug. Closes #634

--- src/game/server/gamecontroller.h
+++ src/game/server/gamecontroller.h
@@ -39,7 +39,6 @@ protected:
 
 	float EvaluateSpawnPos(CSpawnEval *pEval, vec2 Pos);
 	void EvaluateSpawnType(CSpawnEval *pEval, int Type);
-	void FindFreeSpawn(CSpawnEval *pEval, int Type);
 	bool EvaluateSpawn(class CPlayer *pP, vec2 *pPos);
 
 	void CycleMap();
