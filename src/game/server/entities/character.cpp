e53662ea595ce213c9352d1c23c40f26691eb3a8
nsinreal <nsinreal@gmail.com>
2011-06-26 14:17:56 +0300

    Fixed ninja: always set last active weapon after ninja's end

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -120,8 +120,6 @@ void CCharacter::HandleNinja()
 		// time's up, return
 		m_aWeapons[WEAPON_NINJA].m_Got = false;
 		m_ActiveWeapon = m_LastWeapon;
-		if(m_ActiveWeapon == WEAPON_NINJA)
-			m_ActiveWeapon = WEAPON_GUN;
 
 		SetWeapon(m_ActiveWeapon);
 		return;
@@ -486,7 +484,8 @@ void CCharacter::GiveNinja()
 	m_Ninja.m_ActivationTick = Server()->Tick();
 	m_aWeapons[WEAPON_NINJA].m_Got = true;
 	m_aWeapons[WEAPON_NINJA].m_Ammo = -1;
-	m_LastWeapon = m_ActiveWeapon;
+	if (m_ActiveWeapon != WEAPON_NINJA)
+		m_LastWeapon = m_ActiveWeapon;
 	m_ActiveWeapon = WEAPON_NINJA;
 
 	GameServer()->CreateSound(m_Pos, SOUND_PICKUP_NINJA);
