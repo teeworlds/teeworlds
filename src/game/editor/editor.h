ccea5e4ce169c1240dcf2ce1e299421d09c0dfc7
SushiTee <weichel.sascha@xxx.xx>
2011-07-10 22:16:16 +0200

    added grid to editor

--- src/game/editor/editor.h
+++ src/game/editor/editor.h
@@ -514,6 +514,9 @@ public:
 		m_Dialog = 0;
 		m_pTooltip = 0;
 
+		m_GridActive = false;
+		m_GridFactor = 1;
+
 		m_aFileName[0] = 0;
 		m_aFileSaveName[0] = 0;
 		m_ValidSaveFilename = false;
@@ -599,6 +602,9 @@ public:
 	int m_Mode;
 	int m_Dialog;
 	const char *m_pTooltip;
+
+	bool m_GridActive;
+	int m_GridFactor;
 
 	char m_aFileName[512];
 	char m_aFileSaveName[512];
@@ -718,6 +724,8 @@ public:
 
 	void RenderBackground(CUIRect View, int Texture, float Size, float Brightness);
 
+	void RenderGrid(CLayerGroup *pGroup);
+
 	void UiInvokePopupMenu(void *pID, int Flags, float X, float Y, float W, float H, int (*pfnFunc)(CEditor *pEditor, CUIRect Rect), void *pExtra=0);
 	void UiDoPopupMenu();
 
@@ -788,9 +796,11 @@ public:
 		int Length = pEnd > pExtractedName ? min(BufferSize, (int)(pEnd-pExtractedName+1)) : BufferSize;
 		str_copy(pName, pExtractedName, Length);
 	}
-	
+
+	int GetLineDistance();
+
 	unsigned char m_TeleNum;
-	
+
 	unsigned char m_SpeedupForce;
 	short m_SpeedupAngle;
 };
