fd47065aadd26e66ab6fc60994b6b305fddf2f74
SushiTee <weichel.sascha@xxx.xx>
2011-07-12 23:31:39 +0200

    added map info

--- src/game/mapitems.h
+++ src/game/mapitems.h
@@ -98,6 +98,15 @@ public:
 	unsigned char m_Reserved;
 };
 
+struct CMapItemInfo
+{
+	int m_Version;
+	int m_Author;
+	int m_MapVersion;
+	int m_Credits;
+	int m_License;
+} ;
+
 class CTeleTile
 {
 public:
