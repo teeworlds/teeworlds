0f11cf014c7a53c20d4432ea26af41123f9f2436
oy <Tom_Adams@web.de>
2011-07-30 18:19:15 +0200

    increased length for help string of temporary commands

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -655,9 +655,9 @@ void CServer::SendRconLineAuthed(const char *pLine, void *pUser)
 void CServer::SendRconCmdAdd(const IConsole::CCommandInfo *pCommandInfo, int ClientID)
 {
 	CMsgPacker Msg(NETMSG_RCON_CMD_ADD);
-	Msg.AddString(pCommandInfo->m_pName, 32);
-	Msg.AddString(pCommandInfo->m_pHelp, 64);
-	Msg.AddString(pCommandInfo->m_pParams, 16);
+	Msg.AddString(pCommandInfo->m_pName, IConsole::TEMPCMD_NAME_LENGTH);
+	Msg.AddString(pCommandInfo->m_pHelp, IConsole::TEMPCMD_HELP_LENGTH);
+	Msg.AddString(pCommandInfo->m_pParams, IConsole::TEMPCMD_PARAMS_LENGTH);
 	SendMsgEx(&Msg, MSGFLAG_VITAL, ClientID, true);
 }
 
