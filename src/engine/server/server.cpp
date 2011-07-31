3209aec45f61a16423c02545b8b4e34eb6324b60
oy <Tom_Adams@web.de>
2011-07-31 13:17:38 +0200

    fixed compiling error on osx

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -1,6 +1,7 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 
+#include <base/math.h>
 #include <base/system.h>
 
 #include <engine/config.h>
