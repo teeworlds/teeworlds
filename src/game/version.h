72eb98c75ee3f5d8ac87b2ffc3375ca1b329bf72
oy <Tom_Adams@web.de>
2011-08-03 23:17:26 +0200

    changed to trunk again

--- src/game/version.h
+++ src/game/version.h
@@ -3,6 +3,6 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_VERSION "0.6.1"
+#define GAME_VERSION "0.6 trunk"
 #define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
 #endif
