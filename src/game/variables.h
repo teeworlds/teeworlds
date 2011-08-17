acf03d5c2babc4afc19201f96b54b932a591fc07
eeeee <eeeee@qwe123.info>
2011-08-17 03:51:31 +0200

    sv_scoring_debug_log = 1 by default in debug build

--- src/game/variables.h
+++ src/game/variables.h
@@ -87,7 +87,11 @@ MACRO_CONFIG_STR(SvAnnouncement, sv_announcement, 255, "", CFGFLAG_SERVER, "")
 MACRO_CONFIG_INT(SvAnnouncementInterval, sv_announcement_interval, 5, 1, 300, CFGFLAG_SERVER, "")
 
 MACRO_CONFIG_INT(SvScoringDebug, sv_scoring_debug, 0, 0, 1, CFGFLAG_SERVER, "Include scoring debug info in players' names")
+#ifdef CONF_DEBUG
+MACRO_CONFIG_INT(SvScoringDebugLog, sv_scoring_debug_log, 1, 0, 1, CFGFLAG_SERVER, "Include scoring debug info in log")
+#else
 MACRO_CONFIG_INT(SvScoringDebugLog, sv_scoring_debug_log, 0, 0, 1, CFGFLAG_SERVER, "Include scoring debug info in log")
+#endif
 
 MACRO_CONFIG_INT(SvStrictSpectateMode, sv_strict_spectate_mode, 0, 0, 1, CFGFLAG_SERVER, "Restricts information in spectator mode")
 MACRO_CONFIG_INT(SvVoteSpectate, sv_vote_spectate, 1, 0, 1, CFGFLAG_SERVER, "Allow voting to move players to spectators")
