33fafc3eb3e5104512c8c33d23ad996f89ac2773
oy <Tom_Adams@web.de>
2011-11-29 22:18:40 +0100

    added a todo note

--- src/engine/client/serverbrowser.cpp
+++ src/engine/client/serverbrowser.cpp
@@ -1,6 +1,6 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
-#include <algorithm> // sort
+#include <algorithm> // sort  TODO: remove this
 
 #include <base/math.h>
 #include <base/system.h>
