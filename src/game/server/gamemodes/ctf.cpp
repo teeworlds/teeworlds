353f5cc8d69ced0e9d6c50e68ab1678e52a1310c
oy <Tom_Adams@web.de>
2011-12-29 13:56:24 +0100

    fixed that the flag grab sound is played several times in a server side demo. Closes #901

--- src/game/server/gamemodes/ctf.cpp
+++ src/game/server/gamemodes/ctf.cpp
@@ -259,6 +259,8 @@ void CGameControllerCTF::Tick()
 						else
 							GameServer()->CreateSoundGlobal(SOUND_CTF_GRAB_PL, c);
 					}
+					// demo record entry
+					GameServer()->CreateSoundGlobal(SOUND_CTF_GRAB_EN, -2);
 					break;
 				}
 			}
