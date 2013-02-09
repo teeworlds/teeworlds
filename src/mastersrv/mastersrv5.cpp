b1714dde0c6f7a11de05e71f855f096e9326012f
heinrich5991 <heinrich5991@gmail.com>
2013-02-09 15:22:47 +0100

    re-added 0.5 masterserver

--- src/mastersrv/mastersrv5.cpp
+++ src/mastersrv/mastersrv5.cpp
@@ -2,14 +2,20 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 
 #include "mastersrv.h"
-/*
-static const unsigned char SERVERBROWSE_HEARTBEAT_LEGACY[] = {255, 255, 255, 255, 'b', 'e', 'a', 't'};
 
-static const unsigned char SERVERBROWSE_GETLIST_LEGACY[] = {255, 255, 255, 255, 'r', 'e', 'q', 't'};
-static const unsigned char SERVERBROWSE_LIST_LEGACY[] = {255, 255, 255, 255, 'l', 'i', 's', 't'};
+static const unsigned char SERVERBROWSE5_HEARTBEAT[]  = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 'b', 'e', 'a', 't'};
 
-static const unsigned char SERVERBROWSE_GETCOUNT_LEGACY[] = {255, 255, 255, 255, 'c', 'o', 'u', 'n'};
-static const unsigned char SERVERBROWSE_COUNT_LEGACY[] = {255, 255, 255, 255, 's', 'i', 'z', 'e'};
+static const unsigned char SERVERBROWSE5_GETLIST[]    = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 'r', 'e', 'q', 't'};
+static const unsigned char SERVERBROWSE5_LIST[]       = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 'l', 'i', 's', 't'};
+
+static const unsigned char SERVERBROWSE5_GETCOUNT[]   = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 'c', 'o', 'u', 'n'};
+static const unsigned char SERVERBROWSE5_COUNT[]      = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 's', 'i', 'z', 'e'};
+
+static const unsigned char SERVERBROWSE5_FWCHECK[]    = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 'f', 'w', '?', '?'};
+static const unsigned char SERVERBROWSE5_FWRESPONSE[] = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 'f', 'w', '!', '!'};
+static const unsigned char SERVERBROWSE5_FWOK[]       = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 'f', 'w', 'o', 'k'};
+static const unsigned char SERVERBROWSE5_FWERROR[]    = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 'f', 'w', 'e', 'r'};
+//                                                       0    1    2    3    4    5    6    7    8    9   10   11   12   13
 
 class CMastersrvSlave_0_5 : public IMastersrvSlaveAdv
 {
@@ -19,13 +25,14 @@ public:
 
 	virtual int BuildPacketStart(void *pData, int MaxLength);
 	virtual int BuildPacketAdd(void *pData, int MaxLength, const NETADDR *pAddr, void *pUserData);
-	virtual int ProcessMessage(int Socket, const CNetChunk *pPacket, TOKEN PacketToken, int PacketVersion);
+	virtual int ProcessMessage(int Socket, const CNetChunk *pPacket, TOKEN PacketToken) { return 0; }
+	virtual int ProcessMessageRaw(int Socket, const NETADDR *pAddress, const void *pData, int DataSize);
 
 	virtual void SendPacket(int PacketType, const NETADDR *pAddr, void *pUserData);
 
 	struct CCountPacketData
 	{
-		unsigned char m_Header[sizeof(SERVERBROWSE_COUNT_LEGACY)];
+		unsigned char m_Header[sizeof(SERVERBROWSE5_COUNT)];
 		unsigned char m_High;
 		unsigned char m_Low;
 	} m_CountData;
@@ -42,7 +49,7 @@ public:
 CMastersrvSlave_0_5::CMastersrvSlave_0_5(IMastersrv *pOwner)
  : IMastersrvSlaveAdv(pOwner, IMastersrv::MASTERSRV_0_5)
 {
-	mem_copy(m_CountData.m_Header, SERVERBROWSE_COUNT_LEGACY, sizeof(m_CountData.m_Header));
+	mem_copy(m_CountData.m_Header, SERVERBROWSE5_COUNT, sizeof(m_CountData.m_Header));
 
 	for(int i = 0; i < NUM_PACKETS; i++)
 	{
@@ -53,74 +60,71 @@ CMastersrvSlave_0_5::CMastersrvSlave_0_5(IMastersrv *pOwner)
 	m_aPackets[PACKET_COUNT].m_DataSize = sizeof(m_CountData);
 	m_aPackets[PACKET_COUNT].m_pData = &m_CountData;
 
-	m_aPackets[PACKET_CHECK].m_DataSize = sizeof(SERVERBROWSE_FWCHECK);
-	m_aPackets[PACKET_CHECK].m_pData = SERVERBROWSE_FWCHECK;
+	m_aPackets[PACKET_CHECK].m_DataSize = sizeof(SERVERBROWSE5_FWCHECK);
+	m_aPackets[PACKET_CHECK].m_pData = SERVERBROWSE5_FWCHECK;
 
-	m_aPackets[PACKET_OK].m_DataSize = sizeof(SERVERBROWSE_FWOK);
-	m_aPackets[PACKET_OK].m_pData = SERVERBROWSE_FWOK;
+	m_aPackets[PACKET_OK].m_DataSize = sizeof(SERVERBROWSE5_FWOK);
+	m_aPackets[PACKET_OK].m_pData = SERVERBROWSE5_FWOK;
 
-	m_aPackets[PACKET_ERROR].m_DataSize = sizeof(SERVERBROWSE_FWERROR);
-	m_aPackets[PACKET_ERROR].m_pData = SERVERBROWSE_FWERROR;
+	m_aPackets[PACKET_ERROR].m_DataSize = sizeof(SERVERBROWSE5_FWERROR);
+	m_aPackets[PACKET_ERROR].m_pData = SERVERBROWSE5_FWERROR;
 
 	dbg_msg("mastersrv", "started mastersrv 0.5");
 }
 
 int CMastersrvSlave_0_5::BuildPacketStart(void *pData, int MaxLength)
 {
-	if(MaxLength < sizeof(SERVERBROWSE_LIST_LEGACY))
+	if(MaxLength < (int)sizeof(SERVERBROWSE5_LIST))
 		return -1;
-	mem_copy(pData, SERVERBROWSE_LIST_LEGACY, sizeof(SERVERBROWSE_LIST_LEGACY));
-	return sizeof(SERVERBROWSE_LIST_LEGACY);
+	mem_copy(pData, SERVERBROWSE5_LIST, sizeof(SERVERBROWSE5_LIST));
+	return sizeof(SERVERBROWSE5_LIST);
 }
 
 int CMastersrvSlave_0_5::BuildPacketAdd(void *pData, int MaxLength, const NETADDR *pAddr, void *pUserData)
 {
-	if(MaxLength < sizeof(CMastersrvAddrLegacy))
+	if(MaxLength < (int)sizeof(CMastersrvAddrLegacy))
 		return -1;
 	NetaddrToMastersrv((CMastersrvAddrLegacy *)pData, pAddr);
 	return sizeof(CMastersrvAddrLegacy);
 }
 
-int CMastersrvSlave_0_5::ProcessMessage(int Socket, const CNetChunk *pPacket, TOKEN PacketToken, int PacketVersion)
+int CMastersrvSlave_0_5::ProcessMessageRaw(int Socket, const NETADDR *pAddress, const void *pData, int DataSize)
 {
-	if(PacketVersion != NET_PACKETVERSION_LEGACY || !(pPacket->m_Flags&NETSENDFLAG_CONNLESS))
-		return 0;
-
 	if(Socket == IMastersrv::SOCKET_OP)
 	{
-		if(pPacket->m_DataSize == sizeof(SERVERBROWSE_HEARTBEAT_LEGACY)+2 &&
-			mem_comp(pPacket->m_pData, SERVERBROWSE_HEARTBEAT_LEGACY, sizeof(SERVERBROWSE_HEARTBEAT_LEGACY)) == 0)
+		if(DataSize == sizeof(SERVERBROWSE5_HEARTBEAT)+2 &&
+			mem_comp(pData, SERVERBROWSE5_HEARTBEAT, sizeof(SERVERBROWSE5_HEARTBEAT)) == 0)
 		{
 			NETADDR Alt;
-			unsigned char *d = (unsigned char *)pPacket->m_pData;
-			Alt = pPacket->m_Address;
-			Alt.port = (d[sizeof(SERVERBROWSE_HEARTBEAT_LEGACY)]<<8)
-				| d[sizeof(SERVERBROWSE_HEARTBEAT_LEGACY)+1];
-			AddCheckserver(&pPacket->m_Address, &Alt, 0);
+			unsigned char *d = (unsigned char *)pData;
+			Alt = *pAddress;
+			Alt.port = (d[sizeof(SERVERBROWSE5_HEARTBEAT)]<<8)
+				| d[sizeof(SERVERBROWSE5_HEARTBEAT)+1];
+			AddCheckserver(pAddress, &Alt, 0);
 			return 1;
 		}
-		else if(pPacket->m_DataSize == sizeof(SERVERBROWSE_GETCOUNT_LEGACY) &&
-			mem_comp(pPacket->m_pData, SERVERBROWSE_GETCOUNT_LEGACY, sizeof(SERVERBROWSE_GETCOUNT_LEGACY)) == 0)
+		else if(DataSize == sizeof(SERVERBROWSE5_GETCOUNT) &&
+			mem_comp(pData, SERVERBROWSE5_GETCOUNT, sizeof(SERVERBROWSE5_GETCOUNT)) == 0)
 		{
 			int Count = m_pOwner->GetCount();
 			m_CountData.m_High = (Count>>8)&0xff;
 			m_CountData.m_Low = Count&0xff;
-			SendPacket(PACKET_COUNT, &pPacket->m_Address, 0);
+			SendPacket(PACKET_COUNT, pAddress, 0);
 			return 1;
 		}
-		else if(pPacket->m_DataSize == sizeof(SERVERBROWSE_GETLIST_LEGACY) &&
-			mem_comp(pPacket->m_pData, SERVERBROWSE_GETLIST_LEGACY, sizeof(SERVERBROWSE_GETLIST_LEGACY)) == 0)
+		else if(DataSize == sizeof(SERVERBROWSE5_GETLIST) &&
+			mem_comp(pData, SERVERBROWSE5_GETLIST, sizeof(SERVERBROWSE5_GETLIST)) == 0)
 		{
-			IMastersrvSlave::SendList(&pPacket->m_Address, 0);
+			IMastersrvSlave::SendList(pAddress, 0);
 			return 1;
 		}
 	}
 	else if(Socket == IMastersrv::SOCKET_CHECKER)
 	{
-		if(pPacket->m_DataSize == sizeof(SERVERBROWSE_FWRESPONSE) &&
-			mem_comp(pPacket->m_pData, SERVERBROWSE_FWRESPONSE, sizeof(SERVERBROWSE_FWRESPONSE)) == 0)
+		if(DataSize == sizeof(SERVERBROWSE5_FWRESPONSE) &&
+			mem_comp(pData, SERVERBROWSE5_FWRESPONSE, sizeof(SERVERBROWSE5_FWRESPONSE)) == 0)
 		{
-			AddServer(&pPacket->m_Address, 0);
+			AddServer(pAddress, 0);
 			return 0; // for 0.6 compatiblity
 		}
 	}
@@ -134,9 +138,8 @@ void CMastersrvSlave_0_5::SendPacket(int PacketType, const NETADDR *pAddr, void 
 {
 	dbg_assert(PacketType >= 0 && PacketType < NUM_PACKETS, "invalid packet type");
 
-	m_aPackets[PacketType].m_Address = *pAddr;
-	m_pOwner->Send((PacketType != PACKET_CHECK) ? IMastersrv::SOCKET_OP : IMastersrv::SOCKET_CHECKER,
-		&m_aPackets[PacketType], NET_TOKEN_NONE, NET_PACKETVERSION_LEGACY);
+	m_pOwner->SendRaw((PacketType != PACKET_CHECK) ? IMastersrv::SOCKET_OP : IMastersrv::SOCKET_CHECKER,
+		pAddr, m_aPackets[PacketType].m_pData, m_aPackets[PacketType].m_DataSize);
 }
 
 void CMastersrvSlave_0_5::NetaddrToMastersrv(CMastersrvAddrLegacy *pOut, const NETADDR *pIn)
@@ -147,9 +150,9 @@ void CMastersrvSlave_0_5::NetaddrToMastersrv(CMastersrvAddrLegacy *pOut, const N
 	pOut->m_aPort[0] = (pIn->port>>0)&0xff; // little endian
 	pOut->m_aPort[1] = (pIn->port>>8)&0xff;
 }
-*/
+
 IMastersrvSlave *CreateSlave_0_5(IMastersrv *pOwner)
 {
-	return 0;//new CMastersrvSlave_0_5(pOwner);
+	return new CMastersrvSlave_0_5(pOwner);
 }
 
