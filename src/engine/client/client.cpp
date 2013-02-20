07c97822bba0f7b59c911aab2d6e85f84785b6ed
oy <Tom_Adams@web.de>
2013-02-20 15:51:46 +0100

    increased sleep time when tw is minimized and made it adjustable via cputhrottle otherwise

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -1945,10 +1945,10 @@ void CClient::Run()
 			break;
 
 		// beNice
-		if(g_Config.m_DbgStress)
+		if(g_Config.m_ClCpuThrottle)
+			thread_sleep(g_Config.m_ClCpuThrottle);
+		else if(g_Config.m_DbgStress || !m_pGraphics->WindowActive())
 			thread_sleep(5);
-		else if(g_Config.m_ClCpuThrottle || !m_pGraphics->WindowActive())
-			thread_sleep(1);
 
 		if(g_Config.m_DbgHitch)
 		{
