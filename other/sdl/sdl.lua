3fcbb40cac71f118a1d4684a582757d2ec93efde
oy <Tom_Adams@web.de>
2014-02-09 18:03:37 +0100

    removed old sdl framework handling on mac

--- other/sdl/sdl.lua
+++ other/sdl/sdl.lua
@@ -6,7 +6,6 @@ SDL = {
 			option.value = false
 			option.use_sdlconfig = false
 			option.use_winlib = 0
-			option.use_osxframework = false
 			option.lib_path = nil
 			
 			if ExecuteSilent("sdl-config") > 0 and ExecuteSilent("sdl-config --cflags") == 0 then
@@ -21,26 +20,13 @@ SDL = {
 				option.value = true
 				option.use_winlib = 64
 			end
-			
-			if platform == "macosx" then
-				option.value = true
-				option.use_osxframework = true
-				option.use_sdlconfig = false
-			end
 		end
 		
 		local apply = function(option, settings)
 			if option.use_sdlconfig == true then
 				settings.cc.flags:Add("`sdl-config --cflags`")
 				settings.link.flags:Add("`sdl-config --libs`")
-			end
-
-			if option.use_osxframework == true then
-				client_settings.link.frameworks:Add("SDL")
-				client_settings.cc.includes:Add("/Library/Frameworks/SDL.framework/Headers")
-			end
-
-			if option.use_winlib > 0 then
+			elseif option.use_winlib > 0 then
 				settings.cc.includes:Add(SDL.basepath .. "/include")
 				if option.use_winlib == 32 then
 					settings.link.libpath:Add(SDL.basepath .. "/lib32")
@@ -56,7 +42,6 @@ SDL = {
 			output:option(option, "value")
 			output:option(option, "use_sdlconfig")
 			output:option(option, "use_winlib")
-			output:option(option, "use_osxframework")
 		end
 		
 		local display = function(option)
@@ -64,7 +49,6 @@ SDL = {
 				if option.use_sdlconfig == true then return "using sdl-config" end
 				if option.use_winlib == 32 then return "using supplied win32 libraries" end
 				if option.use_winlib == 64 then return "using supplied win64 libraries" end
-				if option.use_osxframework == true then return "using osx framework" end
 				return "using unknown method"
 			else
 				if option.required then
