2ddd01f50a1c32892f320b4564f51308163947df
ghost91- <johannes.loher@fg4f.de>
2014-03-23 11:20:19 +0100

    Fixed compiling on OS X and make_release.py. Teeworlds also works with the bundled version of SDL on OS X now.

--- other/sdl/sdl.lua
+++ other/sdl/sdl.lua
@@ -6,6 +6,7 @@ SDL = {
 			option.value = false
 			option.use_sdlconfig = false
 			option.use_winlib = 0
+			option.use_osxframework = false
 			option.lib_path = nil
 			
 			if ExecuteSilent("sdl-config") > 0 and ExecuteSilent("sdl-config --cflags") == 0 then
@@ -20,13 +21,27 @@ SDL = {
 				option.value = true
 				option.use_winlib = 64
 			end
+			
+			if platform == "macosx" then
+				option.value = true
+				option.use_osxframework = true
+				option.use_sdlconfig = false
+			end
 		end
 		
 		local apply = function(option, settings)
 			if option.use_sdlconfig == true then
 				settings.cc.flags:Add("`sdl-config --cflags`")
 				settings.link.flags:Add("`sdl-config --libs`")
-			elseif option.use_winlib > 0 then
+			end
+
+			if option.use_osxframework == true then
+				client_settings.link.frameworks:Add("SDL")
+				client_settings.link.flags:Add("-Wl,-rpath,@loader_path/../Frameworks")
+				client_settings.cc.includes:Add("/Library/Frameworks/SDL.framework/Headers")
+			end
+
+			if option.use_winlib > 0 then
 				settings.cc.includes:Add(SDL.basepath .. "/include")
 				if option.use_winlib == 32 then
 					settings.link.libpath:Add(SDL.basepath .. "/lib32")
@@ -42,6 +57,7 @@ SDL = {
 			output:option(option, "value")
 			output:option(option, "use_sdlconfig")
 			output:option(option, "use_winlib")
+			output:option(option, "use_osxframework")
 		end
 		
 		local display = function(option)
@@ -49,6 +65,7 @@ SDL = {
 				if option.use_sdlconfig == true then return "using sdl-config" end
 				if option.use_winlib == 32 then return "using supplied win32 libraries" end
 				if option.use_winlib == 64 then return "using supplied win64 libraries" end
+				if option.use_osxframework == true then return "using osx framework" end
 				return "using unknown method"
 			else
 				if option.required then
