cea0cdae8fd74e1fcbed58e64804d80dd9857a97
heinrich5991 <heinrich5991@gmail.com>
2014-05-05 16:19:07 +0200

    Add scripts/reconvert_l10n.py to convert l10n back

---
+++ scripts/reconvert_l10n.py
@@ -0,0 +1,32 @@
+import json
+import polib
+import sys
+
+JSON_KEY_TRANSL="translated strings"
+JSON_KEY_OR="or"
+JSON_KEY_TR="tr"
+JSON_KEY_CO="context"
+
+if __name__ == '__main__':
+	po = polib.pofile(open(sys.argv[1]).read())
+	translations = []
+
+	for entry in po:
+		if entry.msgstr:
+			t_entry = {}
+			t_entry[JSON_KEY_OR] = entry.msgid
+			t_entry[JSON_KEY_TR] = entry.msgstr
+			if entry.msgctxt is not None:
+				t_entry[JSON_KEY_CO] = entry.msgctxt
+			translations.append(t_entry)
+
+	result = {JSON_KEY_TRANSL: translations}
+
+	json.dump(
+		result,
+		open(sys.argv[1] + '.json', 'w'),
+		ensure_ascii=False,
+		indent="\t",
+		separators=(',', ': '),
+		sort_keys=True,
+	)
