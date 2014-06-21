3e049ece9e5066d715d80a8beb687408a7f0f2e4
heinrich5991 <heinrich5991@gmail.com>
2014-06-21 19:23:27 +0200

    Add support for multiple files in scripts/reconvert_l10n.py

--- scripts/reconvert_l10n.py
+++ scripts/reconvert_l10n.py
@@ -1,14 +1,13 @@
 import json
 import polib
-import sys
 
 JSON_KEY_TRANSL="translated strings"
 JSON_KEY_OR="or"
 JSON_KEY_TR="tr"
 JSON_KEY_CO="context"
 
-if __name__ == '__main__':
-	po = polib.pofile(open(sys.argv[1], encoding='utf-8').read())
+def reconvert(filename):
+	po = polib.pofile(open(filename, encoding='utf-8').read())
 	translations = []
 
 	for entry in po:
@@ -24,9 +23,14 @@ if __name__ == '__main__':
 
 	json.dump(
 		result,
-		open(sys.argv[1] + '.json', 'w', encoding='utf-8'),
+		open(filename + '.json', 'w', encoding='utf-8'),
 		ensure_ascii=False,
 		indent="\t",
 		separators=(',', ': '),
 		sort_keys=True,
 	)
+
+if __name__ == '__main__':
+	import sys
+	for filename in sys.argv[1:]:
+		reconvert(filename)
