7a5285cff0e568a551034b4dada57472465bb901
oy <Tom_Adams@web.de>
2015-10-23 09:56:41 +0200

    disable system cursor when in relative mouse mode

--- src/engine/client/input.cpp
+++ src/engine/client/input.cpp
@@ -69,12 +69,14 @@ void CInput::MouseRelative(float *x, float *y)
 void CInput::MouseModeAbsolute()
 {
 	m_InputGrabbed = 0;
+	SDL_ShowCursor(SDL_ENABLE);
 	SDL_SetRelativeMouseMode(SDL_FALSE);
 }
 
 void CInput::MouseModeRelative()
 {
 	m_InputGrabbed = 1;
+	SDL_ShowCursor(SDL_DISABLE);
 	SDL_SetRelativeMouseMode(SDL_TRUE);
 }
 
