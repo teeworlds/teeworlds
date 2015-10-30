47cd70baa325e26672309fca960c9a90f8e89629
Redix <redix@hotmail.de>
2015-10-30 17:21:09 +0100

    fixed ghost include guard

--- src/engine/ghost.h
+++ src/engine/ghost.h
@@ -1,6 +1,6 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
-#ifndef ENGINE_GHOTS_H
+#ifndef ENGINE_GHOST_H
 #define ENGINE_GHOST_H
 
 #include "kernel.h"
