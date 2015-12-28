e3663d6f6a4a7e98ab5226c570d2c3ba746e77a1
Henningstone <Henningstone@users.noreply.github.com>
2015-12-29 00:25:00 +0100

    Fix last commit
    Rename the NetMsg also in the code

--- src/game/client/components/broadcast.cpp
+++ src/game/client/components/broadcast.cpp
@@ -53,9 +53,9 @@ void CBroadcast::OnRender()
 
 void CBroadcast::OnMessage(int MsgType, void *pRawMsg)
 {
-	if(MsgType == NETMSGTYPE_MODAPI_BROADCAST)
+	if(MsgType == NETMSGTYPE_MODAPI_SV_BROADCAST)
 	{
-		CNetMsg_ModAPI_Broadcast *pMsg = (CNetMsg_ModAPI_Broadcast *)pRawMsg;
+		CNetMsg_ModAPI_Sv_Broadcast *pMsg = (CNetMsg_ModAPI_Sv_Broadcast *)pRawMsg;
 		DoBroadcast(pMsg->m_pMessage);
 	}
 }
