TeeworldsFun/teeworlds-TeeUniverse

First draft for animations

datasrc/network.py
@@ -236,6 +236,15 @@ Objects = [
 		NetIntRange("m_Angle", 0, 360),
 	]),
 	
+	NetObject("ModAPI_AnimatedSprite", [
+		NetIntAny("m_AnimatedSpriteId"),
+		NetIntAny("m_X"),
+		NetIntAny("m_Y"),
+		NetIntAny("m_Size"),
+		NetIntRange("m_Angle", 0, 360),
+		NetIntAny("m_StartTick"),
+	]),
+	
 	NetObject("ModAPI_SpriteCharacter", [
 		NetIntAny("m_SpriteId"),
 		NetIntAny("m_ClientId"),

teeworlds/teeworlds

datasrc/network.py
@@ -230,47 +230,71 @@ Objects = [
 	
 	NetObject("ModAPI_Sprite", [
 		NetIntAny("m_SpriteId"),
+		NetIntAny("m_ItemLayer"),
 		NetIntAny("m_X"),
 		NetIntAny("m_Y"),
 		NetIntAny("m_Size"),
 		NetIntRange("m_Angle", 0, 360),
 	]),
 	
-	NetObject("ModAPI_SpriteCharacter", [
-		NetIntAny("m_SpriteId"),
+	NetObject("ModAPI_SpriteCharacter:ModAPI_Sprite", [
 		NetIntAny("m_ClientId"),
+	]),
+	
+	NetObject("ModAPI_AnimatedSprite:ModAPI_Sprite", [
+		NetIntAny("m_AnimationId"),
+		NetIntAny("m_StartTick"),
+		NetIntAny("m_Duration"), #Number of milliseconds for one seconds
+		NetIntAny("m_OffsetX"),
+		NetIntAny("m_OffsetY"),
+	]),
+	
+	NetObject("ModAPI_AnimatedSpriteCharacter:ModAPI_SpriteCharacter", [
+		NetIntAny("m_AnimationId"),
+		NetIntAny("m_StartTick"),
+		NetIntAny("m_Duration"), #Number of milliseconds for one seconds
+		NetIntAny("m_OffsetX"),
+		NetIntAny("m_OffsetY"),
+	]),
+	
+	NetObject("ModAPI_Text", [
+		NetIntAny("m_ItemLayer"),
 		NetIntAny("m_X"),
 		NetIntAny("m_Y"),
+		NetIntAny("m_Alignment"),
+		NetIntAny("m_Color"),
 		NetIntAny("m_Size"),
-		NetIntRange("m_Angle", 0, 360),
+		NetArray(NetIntAny("m_aText"), 16),
+	]),
+	
+	NetObject("ModAPI_TextCharacter:ModAPI_Text", [
+		NetIntAny("m_ClientId"),
+	]),
+	
+	NetObject("ModAPI_AnimatedText:ModAPI_Text", [
+		NetIntAny("m_AnimationId"),
+		NetIntAny("m_StartTick"),
+		NetIntAny("m_Duration"), #Number of milliseconds for one seconds
+		NetIntAny("m_OffsetX"),
+		NetIntAny("m_OffsetY"),
+	]),
+	
+	NetObject("ModAPI_AnimatedTextCharacter:ModAPI_TextCharacter", [
+		NetIntAny("m_AnimationId"),
+		NetIntAny("m_StartTick"),
+		NetIntAny("m_Duration"), #Number of milliseconds for one seconds
+		NetIntAny("m_OffsetX"),
+		NetIntAny("m_OffsetY"),
 	]),
 
 	NetObject("ModAPI_Line", [
 		NetIntAny("m_LineStyleId"),
+		NetIntAny("m_ItemLayer"),
 		NetIntAny("m_StartX"),
 		NetIntAny("m_StartY"),
 		NetIntAny("m_EndX"),
 		NetIntAny("m_EndY"),
 		NetIntAny("m_StartTick"),
-	]),
-	
-	NetObject("ModAPI_Text", [
-		NetIntAny("m_X"),
-		NetIntAny("m_Y"),
-		NetIntAny("m_Alignment"),
-		NetIntAny("m_Color"),
-		NetIntAny("m_Size"),
-		NetArray(NetIntAny("m_aText"), 16),
-	]),
-	
-	NetObject("ModAPI_TextCharacter", [
-		NetIntAny("m_ClientId"),
-		NetIntAny("m_X"),
-		NetIntAny("m_Y"),
-		NetIntAny("m_Alignment"),
-		NetIntAny("m_Color"),
-		NetIntAny("m_Size"),
-		NetArray(NetIntAny("m_aText"), 16),
 	]),
 ]
 