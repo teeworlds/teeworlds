fa6885e4b61fda06a47819abe4e320c54c4076dc
Allan Simon <allan.simon@supinfo.com>
2017-07-27 00:16:46 +0200

    added logs for velocity of players

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -567,6 +567,20 @@ void CCharacter::TickDefered()
 	bool StuckBefore = GameServer()->Collision()->TestBox(m_Core.m_Pos, vec2(28.0f, 28.0f));
 
 	m_Core.Move();
+
+	vec2 EndPost = m_Core.m_Pos;
+
+	char aBuf[256];
+	str_format(
+		aBuf,
+		sizeof(aBuf),
+		"velocity player='%d:%s' value=%f",
+		m_pPlayer->GetCID(),
+		Server()->ClientName(m_pPlayer->GetCID()),
+		distance(EndPost, StartPos)
+	);
+	GameServer()->Console()->Print(IConsole::OUTPUT_LEVEL_DEBUG, "game", aBuf);
+
 	bool StuckAfterMove = GameServer()->Collision()->TestBox(m_Core.m_Pos, vec2(28.0f, 28.0f));
 	m_Core.Quantize();
 	bool StuckAfterQuant = GameServer()->Collision()->TestBox(m_Core.m_Pos, vec2(28.0f, 28.0f));
