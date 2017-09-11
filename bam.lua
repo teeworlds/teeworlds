0df247a925624eaca563f27a1ee5634726faf7f3
oy <Tom_Adams@web.de>
2017-09-11 18:57:17 +0200

    fixed compiling on macosx. raising minimum version to 10.6 as required by sdl

--- bam.lua
+++ bam.lua
@@ -8,7 +8,7 @@ Import("other/freetype/freetype.lua")
 config = NewConfig()
 config:Add(OptCCompiler("compiler"))
 config:Add(OptTestCompileC("stackprotector", "int main(){return 0;}", "-fstack-protector -fstack-protector-all"))
-config:Add(OptTestCompileC("minmacosxsdk", "int main(){return 0;}", "-mmacosx-version-min=10.5 -isysroot /Developer/SDKs/MacOSX10.5.sdk"))
+config:Add(OptTestCompileC("minmacosxsdk", "int main(){return 0;}", "-mmacosx-version-min=10.6 -isysroot /Developer/SDKs/MacOSX10.6.sdk"))
 config:Add(OptLibrary("zlib", "zlib.h", false))
 config:Add(SDL.OptFind("sdl", true))
 config:Add(FreeType.OptFind("freetype", true))
@@ -120,11 +120,11 @@ function GenerateMacOSXSettings(settings, conf, arch, compiler)
 		os.exit(1)
 	end
 
-	settings.cc.flags:Add("-mmacosx-version-min=10.5")
-	settings.link.flags:Add("-mmacosx-version-min=10.5")
+	settings.cc.flags:Add("-mmacosx-version-min=10.6")
+	settings.link.flags:Add("-mmacosx-version-min=10.6")
 	if config.minmacosxsdk.value == 1 then
-		settings.cc.flags:Add("-isysroot /Developer/SDKs/MacOSX10.5.sdk")
-		settings.link.flags:Add("-isysroot /Developer/SDKs/MacOSX10.5.sdk")
+		settings.cc.flags:Add("-isysroot /Developer/SDKs/MacOSX10.6.sdk")
+		settings.link.flags:Add("-isysroot /Developer/SDKs/MacOSX10.6.sdk")
 	end
 
 	settings.link.frameworks:Add("Carbon")
