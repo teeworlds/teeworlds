ddd59eba1fd5fbd56b763cbdd42bf11b05343b5a
清歌 <zyqingjohn@qq.com>
2018-03-14 20:20:41 +0800

    Register more controlable command for dummy
    Which is cl_control_dummy, cl_dummy_jump, cl_dummy_fire and cl_dummy_fire.
    if i want to contol my dummy to jump,
    first, press f1 in game, and use "cl_control_dummy 0",
    second, use "bind mouse1 +toggle cl_dummy_jump 1 0".
    finally, i can contol my dummy to jump by pressing left mouse button.
    
    (cherry picked from commit 06143eb273b116fabd9d030153c6cccb789d05f3)

--- src/game/variables.h
+++ src/game/variables.h
@@ -162,4 +162,10 @@ MACRO_CONFIG_INT(ClDummyHammer, cl_dummy_hammer, 0, 0, 1, CFGFLAG_CLIENT, "Wheth
 MACRO_CONFIG_INT(ClDummyResetOnSwitch, cl_dummy_resetonswitch, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Whether dummy should stop pressing keys when you switch")
 MACRO_CONFIG_INT(ClDummyCopyMoves, cl_dummy_copy_moves, 0, 0, 1, CFGFLAG_CLIENT, "Wether dummy should copy your moves")
 
+// more controlable dummy command
+MACRO_CONFIG_INT(ClControlDummy, cl_control_dummy, 0, 0, 1, CFGFLAG_CLIENT, "Whether can you control dummy at the same time")
+MACRO_CONFIG_INT(ClDummyJump, cl_dummy_jump, 0, 0, 1, CFGFLAG_CLIENT, "Whether dummy is jumping")
+MACRO_CONFIG_INT(ClDummyFire, cl_dummy_fire, 0, 0, 1, CFGFLAG_CLIENT, "Whether dummy is fire")
+MACRO_CONFIG_INT(ClDummyHook, cl_dummy_fire, 0, 0, 1, CFGFLAG_CLIENT, "Whether dummy is hook")
+
 #endif
