a92a7208c00b58efe3b6a68c21ae82f1b5bc9ff2
heinrich5991 <heinrich5991@gmail.com>
2018-07-06 15:45:05 +0200

    Update scripts/check_header_guards.py
    Python 3 compatibility, return code, changing directory before script
    starts.

--- scripts/check_header_guards.py
+++ scripts/check_header_guards.py
@@ -1,35 +1,41 @@
+#!/usr/bin/env python3
 import os
+import sys
 
+os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])) + "/..")
 
-PATH = "../src/"
-
+PATH = "src/"
 
 def check_file(filename):
-	file = open(filename)
-	while 1:
-		line = file.readline()
-		if len(line) == 0:
+	error = False
+	with open(filename) as file:
+		for line in file:
+			if len(line) == 0:
+				break
+			if line[0] == "/" or line[0] == "*" or line[0] == "\r" or line[0] == "\n" or line[0] == "\t":
+				continue
+			if line.startswith("#ifndef"):
+				hg = "#ifndef " + ("_".join(filename.split(PATH)[1].split("/"))[:-2]).upper() + "_H"
+				if line[:-1] != hg:
+					error = True
+					print("Wrong header guard in {}".format(filename))
+			else:
+				error = True
+				print("Missing header guard in {}".format(filename))
 			break
-		if line[0] == "/" or line[0] == "*" or line[0] == "\r" or line[0] == "\n" or line[0] == "\t":
-			continue
-		if line[:7] == "#ifndef":
-			hg = "#ifndef " + ("_".join(filename.split(PATH)[1].split("/"))[:-2]).upper() + "_H"
-			if line[:-1] != hg:
-				print "Wrong header guard in " + filename
-		else:
-			print "Missing header guard in " + filename
-		break
-	file.close()
-
-
+	return error
 
 def check_dir(dir):
+	errors = 0
 	list = os.listdir(dir)
 	for file in list:
-		if os.path.isdir(dir+file):
+		path = dir + file
+		if os.path.isdir(path):
 			if file != "external" and file != "generated":
-				check_dir(dir+file+"/")
-		elif file[-2:] == ".h" and file != "keynames.h":
-			check_file(dir+file)
+				errors += check_dir(path + "/")
+		elif file.endswith(".h") and file != "keynames.h":
+			errors += check_file(path)
+	return errors
 
-check_dir(PATH)
+if __name__ == '__main__':
+	sys.exit(int(check_dir(PATH) != 0))
