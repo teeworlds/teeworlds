99d300f2f4bd24739fd2a13c5e6aff55fbafa6fb
oy <Tom_Adams@web.de>
2018-09-16 17:54:11 +0200

    made 0.6.5 release

--- src/game/version.h
+++ src/game/version.h
@@ -3,7 +3,7 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_VERSION "0.6.4"
+#define GAME_VERSION "0.6.5"
 #define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
-static const char GAME_RELEASE_VERSION[8] = {'0', '.', '6', '.', '4', 0};
+static const char GAME_RELEASE_VERSION[8] = {'0', '.', '6', '.', '5', 0};
 #endif
