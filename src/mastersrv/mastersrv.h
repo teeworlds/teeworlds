1db166cd180446e570484d45463e1f7e65edd2dd
oy <Tom_Adams@web.de>
2018-10-13 14:45:57 +0200

    changed master server port

--- src/mastersrv/mastersrv.h
+++ src/mastersrv/mastersrv.h
@@ -2,7 +2,7 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef MASTERSRV_MASTERSRV_H
 #define MASTERSRV_MASTERSRV_H
-static const int MASTERSERVER_PORT = 8300;
+static const int MASTERSERVER_PORT = 8283;
 
 enum ServerType
 {
