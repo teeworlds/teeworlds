fd142e054ba489ecd59b0cfa8c1791149ffb5bce
Jordy Ruiz <jordy.ruiz@univ-lille.fr>
2018-10-26 14:17:51 +0200

    Fixed highlighting not being triggered when message is empty, after a colon

--- src/game/client/components/chat.cpp
+++ src/game/client/components/chat.cpp
@@ -427,8 +427,11 @@ void CChat::AddLine(int ClientID, int Mode, const char *pLine)
 		if(pHL)
 		{
 			int Length = str_length(m_pClient->m_aClients[m_pClient->m_LocalClientID].m_aName);
-			if((pLine == pHL || pHL[-1] == ' ') && (pHL[Length] == 0 || pHL[Length] == ' ' || (pHL[Length] == ':' && pHL[Length+1] == ' ')))
+			if((pLine == pHL || pHL[-1] == ' ') // space or nothing before
+				&& ((pHL[Length] == 0 || pHL[Length] == ' ') || pHL[Length] == ':' && (pHL[Length+1] == 0) || pHL[Length+1] == ' ')) // space or nothing after, allowing a colon
+			{
 				Highlighted = true;
+			}
 			m_CompletionFav = ClientID;
 		}
 
