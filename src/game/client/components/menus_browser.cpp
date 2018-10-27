fd99a6f7c952f9d643a4d7890a81401973c77b7d
oy <Tom_Adams@web.de>
2018-10-27 17:51:19 +0200

    fixed the delete friend button in the friends browser #1571

--- src/game/client/components/menus_browser.cpp
+++ src/game/client/components/menus_browser.cpp
@@ -2100,8 +2100,7 @@ void CMenus::DoFriendListEntry(CUIRect *pView, CFriendItem *pFriend, const void 
 		{
 			// delete button
 			Button.Margin(1.0f, &Button);
-			static int s_DeleteButton = 0;
-			if(DoButton_SpriteCleanID(&s_DeleteButton, IMAGE_FRIENDICONS, pFriend->IsClanFriend() ? SPRITE_FRIEND_X_A : SPRITE_FRIEND_X_B, &Button, false))
+			if(DoButton_SpriteClean(IMAGE_FRIENDICONS, pFriend->IsClanFriend() ? SPRITE_FRIEND_X_A : SPRITE_FRIEND_X_B, &Button))
 			{
 				m_pDeleteFriendInfo = pFriend->m_pFriendInfo;
 			}
