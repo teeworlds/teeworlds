44a8355e7ae079cce27428c6e1e9aa236eab17c1
LordSk <lordskelethom@gmail.com>
2018-12-01 18:11:18 +0100

    Fix after rebase

--- src/game/editor/editor.cpp
+++ src/game/editor/editor.cpp
@@ -1,3 +1,4 @@
+#if 0
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 
@@ -4730,3 +4731,4 @@ void CEditor::UpdateAndRender()
 }
 
 IEditor *CreateEditor() { return new CEditor; }
+#endif
