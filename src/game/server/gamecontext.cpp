6c3e2713deb227c10767c54eee8554fdf368ad03
oy <Tom_Adams@web.de>
2018-12-01 14:24:00 +0100

    fixed server crash

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -681,8 +681,6 @@ void CGameContext::OnClientDrop(int ClientID, const char *pReason)
 {
 	AbortVoteOnDisconnect(ClientID);
 	m_pController->OnPlayerDisconnect(m_apPlayers[ClientID]);
-	delete m_apPlayers[ClientID];
-	m_apPlayers[ClientID] = 0;
 
 	// update clients on drop
 	if(Server()->ClientIngame(ClientID))
@@ -703,6 +701,9 @@ void CGameContext::OnClientDrop(int ClientID, const char *pReason)
 			Msg.m_Silent = true;
 		Server()->SendPackMsg(&Msg, MSGFLAG_VITAL|MSGFLAG_NORECORD, -1);
 	}
+
+	delete m_apPlayers[ClientID];
+	m_apPlayers[ClientID] = 0;
 
 	m_VoteUpdate = true;
 }
