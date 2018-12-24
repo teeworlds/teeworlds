5754796af1e1e6b894aec952f4caa125c3d085aa
oy <Tom_Adams@web.de>
2018-12-24 10:56:33 +0100

    updated version to 0.7.2

--- src/game/version.h
+++ src/game/version.h
@@ -3,8 +3,8 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include <generated/nethash.cpp>
-#define GAME_VERSION "0.7.1"
+#define GAME_VERSION "0.7.2"
 #define GAME_NETVERSION "0.7 " GAME_NETVERSION_HASH
-#define CLIENT_VERSION 0x0701
-static const char GAME_RELEASE_VERSION[8] = {'0', '.', '7', '.', '1', 0};
+#define CLIENT_VERSION 0x0702
+static const char GAME_RELEASE_VERSION[8] = "0.7.2";
 #endif
