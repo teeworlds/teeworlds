079fe080b10b84640cd28c64e9e6daf6fee8f719
Jordy Ruiz <jordy.ruiz@univ-lille.fr>
2019-01-06 12:12:05 +0100

    Remove scoreboard one-letter locales and add context to server difficulty locales

--- src/game/client/components/menus_browser.cpp
+++ src/game/client/components/menus_browser.cpp
@@ -1841,7 +1841,7 @@ void CMenus::RenderDetailInfo(CUIRect View, const CServerInfo *pInfo)
 	if(pInfo)
 	{
 		CUIRect Row;
-		// Localize("Map"); Localize("Game type"); Localize("Version"); Localize("Casual"); Localize("Normal"); Localize("Difficulty"); Localize("Competitive");
+		// Localize("Map"); Localize("Game type"); Localize("Version"); Localize("Casual", "Server difficulty"); Localize("Normal", "Server difficulty"); Localize("Competitive", "Server difficulty"); Localize("Difficulty");
 		static CLocConstString s_aLabels[] = {
 			"Map",		
 			"Game type",
