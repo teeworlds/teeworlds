bc430595eb71f00bbb6324f75c0a30ababe3cf57
Jordy Ruiz <jordy.ruiz@univ-lille.fr>
2019-03-08 19:18:37 +0100

    Abort in RenderSoundNotification if no active notification

--- src/game/client/components/notifications.cpp
+++ src/game/client/components/notifications.cpp
@@ -38,6 +38,10 @@ void CNotifications::RenderSoundNotification()
 	const float ItemWidth = 20.f;
 	const float DisplayTime = 1.5f; // includes FadeTime
 	const float FadeTime = 0.6f;
+	const float RemainingDisplayTime = max(0.0f, m_SoundToggleTime + DisplayTime - Client()->LocalTime());
+
+	if(RemainingDisplayTime == 0.0f)
+		return;
 
 	Graphics()->MapScreen(0.0f, 0.0f, Width, Height);
 
@@ -47,8 +51,7 @@ void CNotifications::RenderSoundNotification()
 	Area.w = ItemWidth;
 	Area.h = ItemHeight;
 
-	float Fade = max(0.0f, m_SoundToggleTime + DisplayTime - Client()->LocalTime());
-	Fade = min(1.0f, Fade / FadeTime); // 0.0 ≤ Fade ≤ 1.0
+	const float Fade = min(1.0f, RemainingDisplayTime / FadeTime); // 0.0 ≤ Fade ≤ 1.0
 
 	vec4 Color = (g_Config.m_SndEnable == 0) ? vec4(1.f/0xff*0xf9, 1.f/0xff*0x2b, 1.f/0xff*0x2b, 0.55f) : vec4(1.f/0xff*0x2b, 1.f/0xff*0xf9, 1.f/0xff*0x2b, 0.55f);
 	Color = mix(vec4(Color.r, Color.g, Color.b, 0.0f), Color, 0.8*Fade);
