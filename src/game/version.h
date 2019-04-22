e0d0bf1c3850416140260e50400c89267b0588c7
oy <Tom_Adams@web.de>
2019-04-22 18:47:55 +0200

    made 0.7.3.1 release

--- src/game/version.h
+++ src/game/version.h
@@ -6,5 +6,5 @@
 #define GAME_VERSION "0.7.3"
 #define GAME_NETVERSION "0.7 " GAME_NETVERSION_HASH
 #define CLIENT_VERSION 0x0703
-static const char GAME_RELEASE_VERSION[8] = "0.7.3";
+static const char GAME_RELEASE_VERSION[8] = "0.7.3.1";
 #endif
