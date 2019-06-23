1037491b3506d728ac82a461a8eb8a2e955a5d97
ChillerDragon <chillerdragon@gmail.com>
2019-06-23 18:48:10 +0200

    Save downloaded maps to extra folder
    Similar to https://github.com/zillyhuhn/zillywoods/commit/16a2b8bcf8d913cd42817b945e8182375fc1ea2b
    This commit should be reverted somewehen in the future.
    But since currently most people play 0.6 and 0.7 this avoids a mix of non compatible data.

--- src/engine/shared/storage.cpp
+++ src/engine/shared/storage.cpp
@@ -71,6 +71,7 @@ public:
 					fs_makedir(GetPath(TYPE_SAVE, "screenshots/auto", aPath, sizeof(aPath)));
 					fs_makedir(GetPath(TYPE_SAVE, "maps", aPath, sizeof(aPath)));
 					fs_makedir(GetPath(TYPE_SAVE, "downloadedmaps", aPath, sizeof(aPath)));
+					fs_makedir(GetPath(TYPE_SAVE, "downloadedmaps-zilly", aPath, sizeof(aPath)));
 					fs_makedir(GetPath(TYPE_SAVE, "skins", aPath, sizeof(aPath)));
 				}
 				fs_makedir(GetPath(TYPE_SAVE, "dumps", aPath, sizeof(aPath)));
