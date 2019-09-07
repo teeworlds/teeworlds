626df5d1eef47986f4ecd6f7505d72ff06bb0082
Jordy Ruiz <jordy.ruiz@univ-lille.fr>
2019-09-07 12:56:21 +0200

    Add missing Localize for sounds toggling

--- src/game/client/components/menus_callback.cpp
+++ src/game/client/components/menus_callback.cpp
@@ -54,7 +54,7 @@ static CKeyInfo gs_aKeys[] =
 	Localize("Pistol");Localize("Shotgun");Localize("Grenade");Localize("Laser");Localize("Next weapon");Localize("Prev. weapon");
 	Localize("Vote yes");Localize("Vote no");Localize("Chat");Localize("Team chat");Localize("Whisper");Localize("Show chat");Localize("Emoticon");
 	Localize("Spectator mode");Localize("Spectate next");Localize("Spectate previous");Localize("Console");Localize("Remote console");
-	Localize("Screenshot");Localize("Scoreboard");Localize("Statboard");Localize("Respawn");Localize("Ready");Localize("Add demo marker");
+	Localize("Screenshot");Localize("Scoreboard");Localize("Statboard");Localize("Respawn");Localize("Ready");Localize("Add demo marker"); Localize("Toggle sounds")
 */
 
 const int g_KeyCount = sizeof(gs_aKeys) / sizeof(CKeyInfo);
