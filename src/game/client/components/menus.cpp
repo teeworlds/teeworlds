b9db1f1ba8527b5c517ab4e7288bfbc0b37b0ce5
oy <Tom_Adams@web.de>
2019-12-07 18:46:03 +0100

    fixed arrow handling in the demo browser #2253

--- src/game/client/components/menus.cpp
+++ src/game/client/components/menus.cpp
@@ -983,6 +983,15 @@ void CMenus::UiDoListboxStart(CListBoxState* pState, const void *pID, float RowH
 	pState->m_ListBoxDoneEvents = 0;
 	pState->m_ListBoxItemActivated = false;
 
+	// handle input
+	int NewIndex = -1;
+	if(m_DownArrowPressed)
+		NewIndex = pState->m_ListBoxNewSelected + 1;
+	if(m_UpArrowPressed)
+		NewIndex = pState->m_ListBoxNewSelected - 1;
+	if(NewIndex > -1 && NewIndex < pState->m_ListBoxNumItems)
+		pState->m_ListBoxNewSelected = NewIndex;
+
 	// setup the scrollbar
 	pState->m_ScrollOffset = vec2(0, 0);
 	BeginScrollRegion(&pState->m_ScrollRegion, &pState->m_ListBoxView, &pState->m_ScrollOffset);
@@ -997,6 +1006,8 @@ CMenus::CListboxItem CMenus::UiDoListboxNextRow(CListBoxState* pState)
 	if(pState->m_ListBoxItemIndex%pState->m_ListBoxItemsPerRow == 0)
 		pState->m_ListBoxView.HSplitTop(pState->m_ListBoxRowHeight /*-2.0f*/, &s_RowView, &pState->m_ListBoxView);
 	ScrollRegionAddRect(&pState->m_ScrollRegion, s_RowView);
+	if(pState->m_ListBoxNewSelected != pState->m_ListBoxSelectedIndex && pState->m_ListBoxNewSelected == pState->m_ListBoxItemIndex)
+		ScrollRegionScrollHere(&pState->m_ScrollRegion, CScrollRegion::SCROLLHERE_KEEP_IN_VIEW);
 
 	s_RowView.VSplitLeft(s_RowView.w/(pState->m_ListBoxItemsPerRow-pState->m_ListBoxItemIndex%pState->m_ListBoxItemsPerRow), &Item.m_Rect, &s_RowView);
 
