b7e76314023664658b9f119bb80c9bf3d7c26361
oy <Tom_Adams@web.de>
2020-01-12 18:45:01 +0100

    reset checkpoint time on own death in race gametype. closes #2351

--- src/game/client/components/hud.cpp
+++ src/game/client/components/hud.cpp
@@ -784,6 +784,13 @@ void CHud::OnMessage(int MsgType, void *pRawMsg)
 		m_CheckpointDiff = pMsg->m_Diff;
 		m_CheckpointTime = time_get();
 	}
+	else if(MsgType == NETMSGTYPE_SV_KILLMSG && (m_pClient->m_GameInfo.m_GameFlags&GAMEFLAG_RACE))
+	{
+		// reset checkpoint time on death
+		CNetMsg_Sv_KillMsg *pMsg = (CNetMsg_Sv_KillMsg *)pRawMsg;
+		if(pMsg->m_Victim == m_pClient->m_LocalClientID)
+			m_CheckpointTime = 0;
+	}
 }
 
 void CHud::OnRender()
