nonosch07/Blockworlds

Fix client crash when holding space and adding a group in editor

src/game/editor/editor.cpp
@@ -1872,7 +1872,7 @@ void CEditor::DoMapEditor(CUIRect View, CUIRect ToolBar)
 	int NumEditLayers = 0;
 	NumEditLayers = 0;
 
-	if(m_ShowPicker && GetSelectedLayer(0)->m_Type == LAYERTYPE_TILES)
+	if(m_ShowPicker && GetSelectedLayer(0) && GetSelectedLayer(0)->m_Type == LAYERTYPE_TILES)
 	{
 		pEditLayers[0] = &m_TilesetPicker;
 		NumEditLayers++;

teeworlds/teeworlds

(cherry picked from commit https://github.com/ddnet/ddnet/commit/3edd1bb53f52acaabdab05ad84e4f447c723e34e)

src/game/editor/editor.cpp
@@ -1892,7 +1892,7 @@ void CEditor::DoMapEditor(CUIRect View, CUIRect ToolBar)
 	CLayer *pEditLayers[16];
 	int NumEditLayers = 0;
 
-	if(m_ShowTilePicker && GetSelectedLayer(0)->m_Type == LAYERTYPE_TILES)
+	if(m_ShowTilePicker && GetSelectedLayer(0) && GetSelectedLayer(0)->m_Type == LAYERTYPE_TILES)
 	{
 		pEditLayers[0] = &m_TilesetPicker;
 		NumEditLayers++;