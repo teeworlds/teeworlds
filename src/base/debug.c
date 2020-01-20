8e750e0850e23c4614936ed728d387eb803cb7f1
Robert Müller <robert.mueller@student.uni-siegen.de>
2020-01-20 21:16:23 +0100

    add missing imports to debug.c

--- src/base/debug.c
+++ src/base/debug.c
@@ -1,6 +1,9 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
 #include "strings.h"
 #include "time.h"
 #include "debug.h"
