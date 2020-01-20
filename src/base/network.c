aa04cd1152d50736c749bdd6cec8bd33d26370c7
Robert Müller <robert.mueller@student.uni-siegen.de>
2020-01-20 21:32:39 +0100

    add missing imports

--- src/base/network.c
+++ src/base/network.c
@@ -1,6 +1,8 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #include "detect.h"
 #include "memory.h"
