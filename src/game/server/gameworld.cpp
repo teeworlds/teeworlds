6460336328236b4bb609c8302f1d70e7d0e199a8
Robert Müller <robert.mueller@student.uni-siegen.de>
2020-01-22 23:59:55 +0100

    call TickPaused when either game world or game controller are paused (closes #1991)

--- src/game/server/gameworld.cpp
+++ src/game/server/gameworld.cpp
@@ -168,7 +168,18 @@ void CGameWorld::Tick()
 	if(m_ResetRequested)
 		Reset();
 
-	if(!m_Paused)
+	if(m_Paused || GameServer()->m_pController->IsGamePaused())
+	{
+		// update all objects
+		for(int i = 0; i < NUM_ENTTYPES; i++)
+			for(CEntity *pEnt = m_apFirstEntityTypes[i]; pEnt; )
+			{
+				m_pNextTraverseEntity = pEnt->m_pNextTypeEntity;
+				pEnt->TickPaused();
+				pEnt = m_pNextTraverseEntity;
+			}
+	}
+	else
 	{
 		// update all objects
 		for(int i = 0; i < NUM_ENTTYPES; i++)
@@ -184,17 +195,6 @@ void CGameWorld::Tick()
 			{
 				m_pNextTraverseEntity = pEnt->m_pNextTypeEntity;
 				pEnt->TickDefered();
-				pEnt = m_pNextTraverseEntity;
-			}
-	}
-	else if(GameServer()->m_pController->IsGamePaused())
-	{
-		// update all objects
-		for(int i = 0; i < NUM_ENTTYPES; i++)
-			for(CEntity *pEnt = m_apFirstEntityTypes[i]; pEnt; )
-			{
-				m_pNextTraverseEntity = pEnt->m_pNextTypeEntity;
-				pEnt->TickPaused();
 				pEnt = m_pNextTraverseEntity;
 			}
 	}
