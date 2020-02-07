ba1d1bd9d1e0d3e2369d19257fc936d28326e818
Robert Müller <robert.mueller@student.uni-siegen.de>
2020-02-07 20:56:45 +0100

    use white text outline color when loading above 50 percent

--- src/game/client/components/menus.cpp
+++ src/game/client/components/menus.cpp
@@ -1264,6 +1264,7 @@ void CMenus::RenderLoading(int WorkedAmount)
 	RenderTools()->DrawRoundRect(&Rect, vec4(0.0f, 0.0f, 0.0f, 0.5f), 40.0f);
 
 	Rect.y += 20;
+	TextRender()->TextOutlineColor(0.0f, 0.0f, 0.0f, 0.3f);
 	TextRender()->TextColor(1.0f, 1.0f, 1.0f, 1.0f);
 	UI()->DoLabel(&Rect, "Teeworlds", 48.0f, CUI::ALIGN_CENTER);
 
@@ -1271,15 +1272,16 @@ void CMenus::RenderLoading(int WorkedAmount)
 	float Spacing = 40.0f;
 	float Rounding = 5.0f;
 	CUIRect FullBar = {x+Spacing, y+h-75.0f, w-2*Spacing, 25.0f};
-	RenderTools()->DrawRoundRect(&FullBar, vec4(1.0f, 1.0f, 1.0f, 0.10f), Rounding);
+	RenderTools()->DrawRoundRect(&FullBar, vec4(1.0f, 1.0f, 1.0f, 0.1f), Rounding);
 	CUIRect FillingBar = FullBar;
 	FillingBar.w = (FullBar.w-2*Rounding)*Percent+2*Rounding;
 	RenderTools()->DrawRoundRect(&FillingBar, vec4(1.0f, 1.0f, 1.0f, 0.75f), Rounding);
 
-	if(Percent > 0.5)
+	if(Percent > 0.5f)
+	{
+		TextRender()->TextOutlineColor(1.0f, 1.0f, 1.0f, 0.7f);
 		TextRender()->TextColor(0.2f, 0.2f, 0.2f, 1.0f);
-	else
-		TextRender()->TextColor(1.0f, 1.0f, 1.0f, 1.0f);
+	}
 	char aBuf[8];
 	str_format(aBuf, sizeof(aBuf), "%d%%", (int)(100*Percent));
 	UI()->DoLabel(&FullBar, aBuf, 20.0f, CUI::ALIGN_CENTER);
