dc9a7174073e32330e3c114717b09f60f855bb7c
oy <Tom_Adams@web.de>
2020-02-12 17:06:58 +0100

    fixed text colour reset. closes # 2466

--- src/game/client/components/chat.cpp
+++ src/game/client/components/chat.cpp
@@ -1458,6 +1458,7 @@ void CChat::HandleCommands(float x, float y, float w)
 					TextRender()->TextEx(&Cursor, Localize("Press Enter to confirm or Esc to cancel"), -1);
 				else
 					TextRender()->TextEx(&Cursor, Localize("Press Tab to select or Esc to cancel"), -1);
+				TextRender()->TextColor(1.0f, 1.0f, 1.0f, 1.0f);
 			}
 		}
 	}
