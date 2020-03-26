233ca9e68ca070ba05d2c1c064b1149ee707c726
Learath <learath2@gmail.com>
2020-03-27 00:28:03 +0300

    Fix #1860

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -2529,14 +2529,8 @@ void CClient::DoVersionSpecificActions()
 	Prediction Latency
 		Upstream latency
 */
-#if defined(CONF_PLATFORM_MACOSX)
-extern "C" int SDL_main(int argc, char **argv_) // ignore_convention
-{
-	const char **argv = const_cast<const char **>(argv_);
-#else
 int main(int argc, const char **argv) // ignore_convention
 {
-#endif
 #if defined(CONF_FAMILY_WINDOWS)
 	bool QuickEditMode = false;
 	for(int i = 1; i < argc; i++) // ignore_convention
