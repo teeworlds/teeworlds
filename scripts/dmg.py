a8e80a9dd5aa35424b2ac410a3a51321a0e961d3
ChillerDragon <ChillerDragon@gmail.com>
2020-07-30 11:54:56 +0200

    Remove insecure tempfile.mktemp

--- scripts/dmg.py
+++ scripts/dmg.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 import os
 import shlex
 import subprocess
-import tempfile
+from tempfile import NamedTemporaryFile
 
 ConfigDmgtools = namedtuple('Config', 'dmg hfsplus newfs_hfs verbose')
 ConfigHdiutil = namedtuple('Config', 'hdiutil verbose')
@@ -54,7 +54,7 @@ class Dmgtools(Dmg):
 	def create(self, dmg, volume_name, directory, symlinks):
 		input_size = sum(os.stat(os.path.join(path, f)).st_size for path, dirs, files in os.walk(directory) for f in files)
 		output_size = max(input_size * 2, 1024**2)
-		hfs = tempfile.mktemp(prefix=dmg + '.', suffix='.hfs')
+		hfs = NamedTemporaryFile(prefix=dmg + '.', suffix='.hfs', delete=False)
 		self._create_hfs(hfs, volume_name, output_size)
 		self._add(hfs, directory)
 		for target, link_name in symlinks:
