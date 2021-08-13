837b5043d591e2948f18b77eac9c64e5af2c6503
Robert Müller <robert.mueller@uni-siegen.de>
2021-08-13 17:42:06 +0200

    organize includes

--- src/game/client/components/skins.cpp
+++ src/game/client/components/skins.cpp
@@ -1,10 +1,8 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
-#include <math.h>
-
 #include <base/color.h>
-#include <base/system.h>
 #include <base/math.h>
+#include <base/system.h>
 
 #include <engine/graphics.h>
 #include <engine/storage.h>
