nonosch07/Blockworlds

Make components member of CGameClient

src/game/client/components/players.h
@@ -4,6 +4,9 @@
 #define GAME_CLIENT_COMPONENTS_PLAYERS_H
 #include <game/client/component.h>
 
+#include <game/client/render.h>
+#include <game/generated/protocol.h>
+
 class CPlayers : public CComponent
 {
 	friend class CGhost;

teeworlds/teeworlds

(cherry picked from commit https://github.com/ddnet/ddnet/commit/845177582134390c99f4a6e845ad7a47b039849c)

src/game/client/components/players.h
@@ -4,6 +4,9 @@
 #define GAME_CLIENT_COMPONENTS_PLAYERS_H
 #include <game/client/component.h>
 
+#include <game/client/render.h>
+#include <engine/shared/protocol.h>
+
 class CPlayers : public CComponent
 {
 	void RenderPlayer(