b37e32f2f626469c9759de095a63872ac4ba201a
Robert Müller <robert.mueller@uni-siegen.de>
2021-10-15 18:29:50 +0200

    remove unnecessary usage of volatile for ReentryGuard

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -735,20 +735,19 @@ void CServer::SendRconLine(int ClientID, const char *pLine)
 
 void CServer::SendRconLineAuthed(const char *pLine, void *pUser, bool Highlighted)
 {
+	static bool s_ReentryGuard = false;
+	if(s_ReentryGuard)
+		return;
+	s_ReentryGuard = true;
+
 	CServer *pThis = (CServer *)pUser;
-	static volatile int ReentryGuard = 0;
-	int i;
-
-	if(ReentryGuard) return;
-	ReentryGuard++;
-
-	for(i = 0; i < MAX_CLIENTS; i++)
+	for(int i = 0; i < MAX_CLIENTS; i++)
 	{
 		if(pThis->m_aClients[i].m_State != CClient::STATE_EMPTY && pThis->m_aClients[i].m_Authed >= pThis->m_RconAuthLevel)
 			pThis->SendRconLine(i, pLine);
 	}
 
-	ReentryGuard--;
+	s_ReentryGuard = false;
 }
 
 void CServer::SendRconCmdAdd(const IConsole::CCommandInfo *pCommandInfo, int ClientID)
