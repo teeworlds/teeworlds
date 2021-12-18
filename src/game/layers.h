ecf3b7296104dbf2aeab7666fe66eb704783bd08
Robert Müller <robert.mueller@uni-siegen.de>
2021-12-18 14:11:20 +0100

    Add null and size checks to CLayers

--- src/game/layers.h
+++ src/game/layers.h
@@ -21,7 +21,7 @@ class CLayers
 
 public:
 	CLayers();
-	void Init(class IKernel *pKernel, class IMap *pMap=0);
+	void Init(class IKernel *pKernel, class IMap *pMap = 0x0);
 	int NumGroups() const { return m_GroupsNum; }
 	int NumLayers() const { return m_LayersNum; }
 	class IMap *Map() const { return m_pMap; }
