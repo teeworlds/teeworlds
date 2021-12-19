e76436f6fa5bcd0f6a2e61bcb2f1d5d99d8e3b1c
Robert Müller <robert.mueller@uni-siegen.de>
2021-12-19 19:15:50 +0100

    Improve error handling of map_resave tool

--- src/tools/map_resave.cpp
+++ src/tools/map_resave.cpp
@@ -6,27 +6,45 @@
 
 int main(int argc, const char **argv)
 {
+	dbg_logger_stdout();
 	cmdline_fix(&argc, &argv);
 
+	if(argc != 3)
+	{
+		dbg_msg("map_resave", "usage: %s old.map new.map", argv[0]);
+		return -1;
+	}
+
 	IStorage *pStorage = CreateStorage("Teeworlds", IStorage::STORAGETYPE_BASIC, argc, argv);
-	if(!pStorage || argc != 3)
+	if(!pStorage)
 		return -1;
 
 	CDataFileReader Reader;
 	if(!Reader.Open(pStorage, argv[1], IStorage::TYPE_ALL))
+	{
+		dbg_msg("map_resave", "failed to open input file '%s': %s", argv[1], Reader.GetError());
 		return -1;
+	}
 
 	CDataFileWriter Writer;
 	if(!Writer.Open(pStorage, argv[2]))
+	{
+		dbg_msg("map_resave", "failed to open output file '%s'", argv[2]);
 		return -1;
+	}
 
 	// add all items
 	for(int Index = 0; Index < Reader.NumItems(); Index++)
 	{
-		int Type, ID;
-		void *pPtr = Reader.GetItem(Index, &Type, &ID);
-		int Size = Reader.GetItemSize(Index);
-		Writer.AddItem(Type, ID, Size, pPtr);
+		int Type, ID, Size;
+		void *pPtr = Reader.GetItem(Index, &Type, &ID, &Size);
+		if(!pPtr)
+		{
+			dbg_msg("map_resave", "failed to get map item %d", Index);
+			continue;
+		}
+		if(Writer.AddItem(Type, ID, Size, pPtr) == -1)
+			dbg_msg("map_resave", "failed to add map item %d (Type=%d, ID=%d, Size=%d)", Index, Type, ID, Size);
 	}
 
 	// add all data
@@ -34,11 +52,19 @@ int main(int argc, const char **argv)
 	{
 		void *pPtr = Reader.GetData(Index);
 		int Size = Reader.GetDataSize(Index);
-		Writer.AddData(Size, pPtr);
+		if(!pPtr || Size <= 0)
+		{
+			dbg_msg("map_resave", "failed to get map data %d", Index);
+			continue;
+		}
+		if(Writer.AddData(Size, pPtr) == -1)
+			dbg_msg("map_resave", "failed to add map data %d (Size=%d)", Index, Size);
 	}
 
 	Reader.Close();
 	Writer.Finish();
+
+	dbg_msg("map_resave", "map successfully resaved to '%s'", argv[2]);
 
 	cmdline_free(argc, argv);
 	return 0;
