fe47b131ee0b622a4af6be1ad3d5015f43c15c49
Robert Müller <robert.mueller@uni-siegen.de>
2021-12-23 18:18:41 +0100

    Validate all CDataFileReader and CDataFileWriter structures

--- src/engine/shared/datafile.h
+++ src/engine/shared/datafile.h
@@ -3,40 +3,45 @@
 #ifndef ENGINE_SHARED_DATAFILE_H
 #define ENGINE_SHARED_DATAFILE_H
 
-#include <base/system.h>
 #include <base/hash.h>
+#include <base/system.h>
 
 // raw datafile access
 class CDataFileReader
 {
 	struct CDatafile *m_pDataFile;
-	void *GetDataImpl(int Index, int Swap);
+	char m_aError[128];
+
+	void *GetDataImpl(int Index, bool Swap);
 	int GetFileDataSize(int Index) const;
 	int GetFileItemSize(int Index) const;
-public:
-	CDataFileReader() : m_pDataFile(0) {}
-	~CDataFileReader() { Close(); }
 
-	bool IsOpen() const { return m_pDataFile != 0; }
+public:
+	CDataFileReader() : m_pDataFile(0) { m_aError[0] = '\0'; }
+	~CDataFileReader() { Close(); }
 
 	bool Open(class IStorage *pStorage, const char *pFilename, int StorageType);
 	bool Close();
 
+	bool IsOpen() const { return m_pDataFile != 0; }
+	const char *GetError() const { return m_aError; }
+
 	void *GetData(int Index);
 	void *GetDataSwapped(int Index); // makes sure that the data is 32bit LE ints when saved
+	bool GetDataString(int Index, char *pBuffer, int BufferSize);
 	int GetDataSize(int Index) const;
 	void ReplaceData(int Index, char *pData, int Size);
 	void UnloadData(int Index);
-	void *GetItem(int Index, int *pType, int *pID);
+	void *GetItem(int Index, int *pType = 0, int *pID = 0, int *pSize = 0) const;
 	int GetItemSize(int Index) const;
-	void GetType(int Type, int *pStart, int *pNum);
-	void *FindItem(int Type, int ID);
+	void GetType(int Type, int *pStart, int *pNum) const;
+	void *FindItem(int Type, int ID, int *pIndex = 0, int *pSize = 0) const;
 	int NumItems() const;
 	int NumData() const;
-	void Unload();
 
 	SHA256_DIGEST Sha256() const;
 	unsigned Crc() const;
+	unsigned FileSize() const;
 
 	static bool CheckSha256(IOHANDLE Handle, const void *pSha256);
 };
@@ -44,53 +49,25 @@ public:
 // write access
 class CDataFileWriter
 {
-	struct CDataInfo
-	{
-		int m_UncompressedSize;
-		int m_CompressedSize;
-		void *m_pCompressedData;
-	};
-
-	struct CItemInfo
-	{
-		int m_Type;
-		int m_ID;
-		int m_Size;
-		int m_Next;
-		int m_Prev;
-		void *m_pData;
-	};
-
-	struct CItemTypeInfo
-	{
-		int m_Num;
-		int m_First;
-		int m_Last;
-	};
-
-	enum
-	{
-		MAX_ITEM_TYPES=0xffff,
-		MAX_ITEMS=1024,
-		MAX_DATAS=1024,
-	};
-
 	IOHANDLE m_File;
+	int m_NumItemTypes;
 	int m_NumItems;
 	int m_NumDatas;
-	int m_NumItemTypes;
-	CItemTypeInfo *m_pItemTypes;
-	CItemInfo *m_pItems;
-	CDataInfo *m_pDatas;
+	struct CItemTypeInfo *m_pItemTypes;
+	struct CItemInfo *m_pItems;
+	struct CDataInfo *m_pDatas;
 
 public:
 	CDataFileWriter();
 	~CDataFileWriter();
+
 	bool Open(class IStorage *pStorage, const char *Filename);
+	bool Finish();
+
+	// These function return the index of the added data/item, or -1 on error.
 	int AddData(int Size, const void *pData);
 	int AddDataSwapped(int Size, const void *pData);
 	int AddItem(int Type, int ID, int Size, const void *pData);
-	int Finish();
 };
 
 
