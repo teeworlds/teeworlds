94586c94736f470409736b6d7d1db48f0f287343
Robert Müller <robert.mueller@uni-siegen.de>
2021-12-30 11:45:56 +0100

    close thread handle in thread_destroy on windows

--- src/base/system.c
+++ src/base/system.c
@@ -536,11 +536,8 @@ void thread_wait(void *thread)
 
 void thread_destroy(void *thread)
 {
-#if defined(CONF_FAMILY_UNIX)
-	void *r = 0;
-	pthread_join((pthread_t)thread, &r);
-#else
-	/*#error not implemented*/
+#if defined(CONF_FAMILY_WINDOWS)
+	CloseHandle((HANDLE)thread);
 #endif
 }
 
