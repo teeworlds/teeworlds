8fc3405978368e6ae12d54a989ba7b83245f8e1a
Robert Müller <robert.mueller@uni-siegen.de>
2022-02-04 20:59:41 +0100

    Enable mapversion sending in versionsrv, improve code style

--- src/versionsrv/versionsrv.cpp
+++ src/versionsrv/versionsrv.cpp
@@ -1,6 +1,7 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include <base/system.h>
+#include <base/math.h>
 
 #include <engine/config.h>
 #include <engine/console.h>
@@ -14,69 +15,86 @@
 #include "versionsrv.h"
 #include "mapversions.h"
 
-enum {
-	MAX_MAPS_PER_PACKET=48,
-	MAX_PACKETS=16,
-	MAX_MAPS=MAX_MAPS_PER_PACKET*MAX_PACKETS,
+enum
+{
+	MAX_MAPS_PER_PACKET = 48,
+	MAX_PACKETS = 16,
+	MAX_MAPS = MAX_MAPS_PER_PACKET * MAX_PACKETS,
 };
 
-struct CPacketData
+struct CMapversionPacketData
 {
 	int m_Size;
-	struct {
+	struct
+	{
 		unsigned char m_aHeader[sizeof(VERSIONSRV_MAPLIST)];
 		CMapVersion m_aMaplist[MAX_MAPS_PER_PACKET];
 	} m_Data;
 };
 
-CPacketData m_aPackets[MAX_PACKETS];
-static int m_NumPackets = 0;
+static CNetClient s_NetClient;
 
-static CNetClient g_NetOp; // main
+static unsigned char s_aVersionPacket[sizeof(VERSIONSRV_VERSION) + sizeof(GAME_RELEASE_VERSION)];
+static CMapversionPacketData s_aMapversionPackets[MAX_PACKETS];
+static int s_NumMapversionPackets = 0;
 
-void BuildPackets()
+static void BuildVersionPacket()
 {
-	CMapVersion *pCurrent = &s_aMapVersionList[0];
-	int ServersLeft = s_NumMapVersionItems;
-	m_NumPackets = 0;
-	while(ServersLeft && m_NumPackets < MAX_PACKETS)
+	mem_copy(s_aVersionPacket, VERSIONSRV_VERSION, sizeof(VERSIONSRV_VERSION));
+	mem_copy(s_aVersionPacket + sizeof(VERSIONSRV_VERSION), GAME_RELEASE_VERSION, sizeof(GAME_RELEASE_VERSION));
+}
+
+static void BuildMapversionPacket()
+{
+	dbg_assert(s_NumMapVersionItems <= MAX_MAPS, "too many maps");
+	const CMapVersion *pCurrent = &s_aMapVersionList[0];
+	int MapsLeft = s_NumMapVersionItems;
+	s_NumMapversionPackets = 0;
+	while(MapsLeft > 0 && s_NumMapversionPackets < MAX_PACKETS)
 	{
-		int Chunk = ServersLeft;
-		if(Chunk > MAX_MAPS_PER_PACKET)
-			Chunk = MAX_MAPS_PER_PACKET;
-		ServersLeft -= Chunk;
+		const int Chunk = minimum<int>(MapsLeft, MAX_MAPS_PER_PACKET);
+		MapsLeft -= Chunk;
 
 		// copy header
-		mem_copy(m_aPackets[m_NumPackets].m_Data.m_aHeader, VERSIONSRV_MAPLIST, sizeof(VERSIONSRV_MAPLIST));
+		mem_copy(s_aMapversionPackets[s_NumMapversionPackets].m_Data.m_aHeader, VERSIONSRV_MAPLIST, sizeof(VERSIONSRV_MAPLIST));
 
 		// copy map versions
 		for(int i = 0; i < Chunk; i++)
 		{
-			m_aPackets[m_NumPackets].m_Data.m_aMaplist[i] = *pCurrent;
+			s_aMapversionPackets[s_NumMapversionPackets].m_Data.m_aMaplist[i] = *pCurrent;
 			pCurrent++;
 		}
 
-		m_aPackets[m_NumPackets].m_Size = sizeof(VERSIONSRV_MAPLIST) + sizeof(CMapVersion)*Chunk;
+		s_aMapversionPackets[s_NumMapversionPackets].m_Size = sizeof(VERSIONSRV_MAPLIST) + sizeof(CMapVersion) * Chunk;
 
-		m_NumPackets++;
+		s_NumMapversionPackets++;
 	}
 }
 
-void SendVer(NETADDR *pAddr, TOKEN ResponseToken)
+static void SendVersion(NETADDR *pAddr, TOKEN ResponseToken)
 {
-	CNetChunk p;
-	unsigned char aData[sizeof(VERSIONSRV_VERSION) + sizeof(GAME_RELEASE_VERSION)];
+	CNetChunk Packet;
+	Packet.m_ClientID = -1;
+	Packet.m_Address = *pAddr;
+	Packet.m_Flags = NETSENDFLAG_CONNLESS;
+	Packet.m_pData = s_aVersionPacket;
+	Packet.m_DataSize = sizeof(s_aVersionPacket);
+	s_NetClient.Send(&Packet, ResponseToken);
+}
 
-	mem_copy(aData, VERSIONSRV_VERSION, sizeof(VERSIONSRV_VERSION));
-	mem_copy(aData + sizeof(VERSIONSRV_VERSION), GAME_RELEASE_VERSION, sizeof(GAME_RELEASE_VERSION));
+static void SendMapversions(NETADDR *pAddr, TOKEN ResponseToken)
+{
+	CNetChunk Packet;
+	Packet.m_ClientID = -1;
+	Packet.m_Address = *pAddr;
+	Packet.m_Flags = NETSENDFLAG_CONNLESS;
 
-	p.m_ClientID = -1;
-	p.m_Address = *pAddr;
-	p.m_Flags = NETSENDFLAG_CONNLESS;
-	p.m_pData = aData;
-	p.m_DataSize = sizeof(aData);
-
-	g_NetOp.Send(&p, ResponseToken);
+	for(int i = 0; i < s_NumMapversionPackets; i++)
+	{
+		Packet.m_DataSize = s_aMapversionPackets[i].m_Size;
+		Packet.m_pData = &s_aMapversionPackets[i].m_Data;
+		s_NetClient.Send(&Packet, ResponseToken);
+	}
 }
 
 int main(int argc, const char **argv)
@@ -84,7 +102,7 @@ int main(int argc, const char **argv)
 	dbg_logger_stdout();
 	cmdline_fix(&argc, &argv);
 
-	int FlagMask = 0;
+	const int FlagMask = 0;
 	IKernel *pKernel = IKernel::Create();
 	IStorage *pStorage = CreateStorage("Teeworlds", IStorage::STORAGETYPE_BASIC, argc, argv);
 	IConfigManager *pConfigManager = CreateConfigManager();
@@ -93,62 +111,55 @@ int main(int argc, const char **argv)
 	bool RegisterFail = !pKernel->RegisterInterface(pStorage);
 	RegisterFail |= !pKernel->RegisterInterface(pConsole);
 	RegisterFail |= !pKernel->RegisterInterface(pConfigManager);
-
 	if(RegisterFail)
 		return -1;
+
 	pConfigManager->Init(FlagMask);
 	pConsole->Init();
 
-	NETADDR BindAddr;
-	mem_zero(&BindAddr, sizeof(BindAddr));
-	BindAddr.type = NETTYPE_ALL;
-	BindAddr.port = VERSIONSRV_PORT;
 	if(secure_random_init() != 0)
 	{
 		dbg_msg("versionsrv", "could not initialize secure RNG");
 		return -1;
 	}
-	if(!g_NetOp.Open(BindAddr, pConfigManager->Values(), pConsole, 0, 0))
+
+	NETADDR BindAddr;
+	mem_zero(&BindAddr, sizeof(BindAddr));
+	BindAddr.type = NETTYPE_ALL;
+	BindAddr.port = VERSIONSRV_PORT;
+	if(!s_NetClient.Open(BindAddr, pConfigManager->Values(), pConsole, 0, 0))
 	{
-		dbg_msg("mastersrv", "couldn't start network");
+		dbg_msg("versionsrv", "could not start network");
 		return -1;
 	}
 
-	BuildPackets();
+	dbg_msg("versionsrv", "building packets");
+
+	BuildVersionPacket();
+	BuildMapversionPacket();
 
 	dbg_msg("versionsrv", "started");
 
-	while(1)
+	while(true)
 	{
-		g_NetOp.Update();
+		s_NetClient.Update();
 
 		// process packets
 		CNetChunk Packet;
 		TOKEN ResponseToken;
-		while(g_NetOp.Recv(&Packet, &ResponseToken))
+		while(s_NetClient.Recv(&Packet, &ResponseToken))
 		{
-			if(Packet.m_DataSize == sizeof(VERSIONSRV_GETVERSION) &&
-				mem_comp(Packet.m_pData, VERSIONSRV_GETVERSION, sizeof(VERSIONSRV_GETVERSION)) == 0)
+			if(Packet.m_DataSize == sizeof(VERSIONSRV_GETVERSION)
+				&& mem_comp(Packet.m_pData, VERSIONSRV_GETVERSION, sizeof(VERSIONSRV_GETVERSION)) == 0)
 			{
-				SendVer(&Packet.m_Address, ResponseToken);
+				SendVersion(&Packet.m_Address, ResponseToken);
 			}
 
-			//disable that for now
-			/*if(Packet.m_DataSize == sizeof(VERSIONSRV_GETMAPLIST) &&
-				mem_comp(Packet.m_pData, VERSIONSRV_GETMAPLIST, sizeof(VERSIONSRV_GETMAPLIST)) == 0)
+			if(Packet.m_DataSize == sizeof(VERSIONSRV_GETMAPLIST)
+				&& mem_comp(Packet.m_pData, VERSIONSRV_GETMAPLIST, sizeof(VERSIONSRV_GETMAPLIST)) == 0)
 			{
-				CNetChunk p;
-				p.m_ClientID = -1;
-				p.m_Address = Packet.m_Address;
-				p.m_Flags = NETSENDFLAG_CONNLESS;
-
-				for(int i = 0; i < m_NumPackets; i++)
-				{
-					p.m_DataSize = m_aPackets[i].m_Size;
-					p.m_pData = &m_aPackets[i].m_Data;
-					g_NetOp.Send(&p, ResponseToken);
-				}
-			}*/
+				SendMapversions(&Packet.m_Address, ResponseToken);
+			}
 		}
 
 		// be nice to the CPU
