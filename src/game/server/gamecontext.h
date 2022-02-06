cbe70697e9de55ca786c1803ea9b67a27b9bb8fe
ChillerDragon <ChillerDragon@gmail.com>
2022-02-06 12:01:48 +0100

    Expose Storage in gamecontext
    This allows server modders to use GameServer()->Storage() in their own
    game controller without touching other source code

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -39,6 +39,7 @@ class CGameContext : public IGameServer
 	IServer *m_pServer;
 	class CConfig *m_pConfig;
 	class IConsole *m_pConsole;
+	class IStorage *m_pStorage;
 	CLayers m_Layers;
 	CCollision m_Collision;
 	CNetObjHandler m_NetObjHandler;
@@ -77,6 +78,7 @@ public:
 	IServer *Server() const { return m_pServer; }
 	class CConfig *Config() { return m_pConfig; }
 	class IConsole *Console() { return m_pConsole; }
+	class IStorage *Storage() { return m_pStorage; }
 	CCollision *Collision() { return &m_Collision; }
 	CTuningParams *Tuning() { return &m_Tuning; }
 
