ba3f39a9c51b6cadc6114de90eb0403ce6383466
Robert Müller <robytemueller@gmail.com>
2022-03-03 23:20:33 +0100

    Add CMsgUnpacker, add validation to CPacker, refactoring

--- src/engine/shared/packer.cpp
+++ src/engine/shared/packer.cpp
@@ -2,26 +2,25 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include <base/system.h>
 
-#include "packer.h"
 #include "compression.h"
-#include "config.h"
+#include "packer.h"
 
 void CPacker::Reset()
 {
-	m_Error = 0;
+	m_Error = false;
 	m_pCurrent = m_aBuffer;
 	m_pEnd = m_pCurrent + PACKER_BUFFER_SIZE;
 }
 
-void CPacker::AddInt(int i)
+void CPacker::AddInt(int Integer)
 {
 	if(m_Error)
 		return;
 
-	unsigned char *pNext = CVariableInt::Pack(m_pCurrent, i, m_pEnd - m_pCurrent);
+	unsigned char *pNext = CVariableInt::Pack(m_pCurrent, Integer, RemainingSize());
 	if(!pNext)
 	{
-		m_Error = 1;
+		m_Error = true;
 		return;
 	}
 	m_pCurrent = pNext;
@@ -33,29 +32,26 @@ void CPacker::AddString(const char *pStr, int Limit)
 		return;
 
 	if(Limit <= 0)
-	{
 		Limit = PACKER_BUFFER_SIZE;
-	}
+
 	while(*pStr && Limit != 0)
 	{
 		int Codepoint = str_utf8_decode(&pStr);
 		if(Codepoint == -1)
-		{
 			Codepoint = 0xfffd; // Unicode replacement character.
-		}
-		char aGarbage[4];
-		int Length = str_utf8_encode(aGarbage, Codepoint);
+
+		char aEncoded[4];
+		const int Length = str_utf8_encode(aEncoded, Codepoint);
 		if(Limit < Length)
-		{
 			break;
-		}
+
 		// Ensure space for the null termination.
-		if(m_pEnd - m_pCurrent < Length + 1)
+		if(RemainingSize() < Length + 1)
 		{
-			m_Error = 1;
+			m_Error = true;
 			break;
 		}
-		Length = str_utf8_encode((char *)m_pCurrent, Codepoint);
+		mem_copy(m_pCurrent, aEncoded, Length);
 		m_pCurrent += Length;
 		Limit -= Length;
 	}
@@ -67,24 +63,20 @@ void CPacker::AddRaw(const void *pData, int Size)
 	if(m_Error)
 		return;
 
-	if(m_pCurrent+Size >= m_pEnd)
+	if(Size <= 0 || Size >= RemainingSize())
 	{
-		m_Error = 1;
+		m_Error = true;
 		return;
 	}
 
-	const unsigned char *pSrc = (const unsigned char *)pData;
-	while(Size)
-	{
-		*m_pCurrent++ = *pSrc++;
-		Size--;
-	}
+	mem_copy(m_pCurrent, pData, Size);
+	m_pCurrent += Size;
 }
 
 
 void CUnpacker::Reset(const void *pData, int Size)
 {
-	m_Error = 0;
+	m_Error = false;
 	m_pStart = (const unsigned char *)pData;
 	m_pEnd = m_pStart + Size;
 	m_pCurrent = m_pStart;
@@ -95,75 +87,84 @@ int CUnpacker::GetInt()
 	if(m_Error)
 		return 0;
 
-	if(m_pCurrent >= m_pEnd)
+	if(RemainingSize() <= 0)
 	{
-		m_Error = 1;
+		m_Error = true;
 		return 0;
 	}
 
-	int i;
-	const unsigned char *pNext = CVariableInt::Unpack(m_pCurrent, &i, m_pEnd - m_pCurrent);
+	int Integer;
+	const unsigned char *pNext = CVariableInt::Unpack(m_pCurrent, &Integer, RemainingSize());
 	if(!pNext)
 	{
-		m_Error = 1;
+		m_Error = true;
 		return 0;
 	}
 	m_pCurrent = pNext;
-	return i;
+	return Integer;
 }
 
 int CUnpacker::GetIntOrDefault(int Default)
 {
 	if(m_Error)
-	{
 		return 0;
-	}
-	if(m_pCurrent == m_pEnd)
-	{
+
+	if(RemainingSize() == 0)
 		return Default;
-	}
+
 	return GetInt();
 }
 
 const char *CUnpacker::GetString(int SanitizeType)
 {
-	if(m_Error || m_pCurrent >= m_pEnd)
+	if(m_Error)
 		return "";
 
-	char *pPtr = (char *)m_pCurrent;
-	while(*m_pCurrent) // skip the string
+	if(RemainingSize() <= 0)
+	{
+		m_Error = true;
+		return "";
+	}
+
+	// Ensure string is null terminated.
+	char *pStr = (char *)m_pCurrent;
+	while(*m_pCurrent)
 	{
 		m_pCurrent++;
 		if(m_pCurrent == m_pEnd)
 		{
-			m_Error = 1;;
+			m_Error = true;
 			return "";
 		}
 	}
 	m_pCurrent++;
 
 	// sanitize all strings
-	if(SanitizeType&SANITIZE)
-		str_sanitize(pPtr);
-	else if(SanitizeType&SANITIZE_CC)
-		str_sanitize_cc(pPtr);
-	return SanitizeType&SKIP_START_WHITESPACES ? str_utf8_skip_whitespaces(pPtr) : pPtr;
+	if(SanitizeType & SANITIZE)
+		str_sanitize(pStr);
+	else if(SanitizeType & SANITIZE_CC)
+		str_sanitize_cc(pStr);
+
+	if(SanitizeType & SKIP_START_WHITESPACES)
+		return str_utf8_skip_whitespaces(pStr);
+
+	return pStr;
 }
 
 const unsigned char *CUnpacker::GetRaw(int Size)
 {
-	const unsigned char *pPtr = m_pCurrent;
 	if(m_Error)
 		return 0;
 
 	// check for nasty sizes
-	if(Size < 0 || m_pCurrent+Size > m_pEnd)
+	if(Size <= 0 || Size > RemainingSize())
 	{
-		m_Error = 1;
+		m_Error = true;
 		return 0;
 	}
 
 	// "unpack" the data
+	const unsigned char *pPtr = m_pCurrent;
 	m_pCurrent += Size;
 	return pPtr;
 }
