2a7ccf3cc8e70ed743ad50bea8a67f6dfe211f4d
Robert Müller <robytemueller@gmail.com>
2022-03-10 21:04:14 +0100

    Add copyright notices to file headers

--- src/base/hash.h
+++ src/base/hash.h
@@ -1,3 +1,5 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef BASE_HASH_H
 #define BASE_HASH_H
 
--- src/test/test.h
+++ src/test/test.h
@@ -1,3 +1,5 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef TEST_TEST_H
 #define TEST_TEST_H
 class CTestInfo
