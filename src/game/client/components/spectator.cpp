6bbeed957f77d4a233e600dedde906312f4fce6e
Robert Müller <robytemueller@gmail.com>
2022-03-13 20:52:01 +0100

    Use SpecModePossible to reduce duplicate code

--- src/game/client/components/spectator.cpp
+++ src/game/client/components/spectator.cpp
@@ -173,13 +173,7 @@ void CSpectator::OnRender()
 	int TotalCount = 0;
 	for(int i = 0; i < MAX_CLIENTS; ++i)
 	{
-		if(!m_pClient->m_Snap.m_paPlayerInfos[i]
-			|| m_pClient->m_aClients[i].m_Team == TEAM_SPECTATORS
-			|| (m_pClient->m_LocalClientID != -1
-				&& m_pClient->m_aClients[m_pClient->m_LocalClientID].m_Team != TEAM_SPECTATORS
-				&& (m_pClient->m_Snap.m_paPlayerInfos[i]->m_PlayerFlags&PLAYERFLAG_DEAD
-					|| m_pClient->m_aClients[m_pClient->m_LocalClientID].m_Team != m_pClient->m_aClients[i].m_Team
-					|| i == m_pClient->m_LocalClientID)))
+		if(!SpecModePossible(SPEC_PLAYER, i))
 			continue;
 		TotalCount++;
 	}
@@ -281,12 +275,7 @@ void CSpectator::OnRender()
 	// draw player selection
 	for(int i = 0, Count = 0; i < MAX_CLIENTS; ++i)
 	{
-		if(!m_pClient->m_Snap.m_paPlayerInfos[i]
-			|| m_pClient->m_aClients[i].m_Team == TEAM_SPECTATORS
-			|| m_pClient->m_Snap.m_paPlayerInfos[i]->m_PlayerFlags&PLAYERFLAG_DEAD
-			|| (m_pClient->m_LocalClientID != -1
-				&& m_pClient->m_aClients[m_pClient->m_LocalClientID].m_Team != TEAM_SPECTATORS
-				&& (m_pClient->m_aClients[m_pClient->m_LocalClientID].m_Team != m_pClient->m_aClients[i].m_Team || i == m_pClient->m_LocalClientID)))
+		if(!SpecModePossible(SPEC_PLAYER, i))
 			continue;
 
 		if(Count != 0 && Count % ColumnSize == 0)
