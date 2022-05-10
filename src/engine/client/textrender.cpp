d2530cb05e0b5499117977b1a9f00f8ed8b8b450
Robert Müller <robytemueller@gmail.com>
2022-05-10 21:20:56 +0200

    Fix clang-analyzer-deadcode.DeadStores
    /home/runner/work/teeworlds/teeworlds/src/game/client/components/menus_settings.cpp:396:2: error: Value stored to 'OldSelected' is never read [clang-analyzer-deadcode.DeadStores,-warnings-as-errors]
            OldSelected = NewSelected;
            ^
    
    /home/runner/work/teeworlds/teeworlds/src/game/client/components/stats.cpp:346:4: error: Value stored to 'px' is never read [clang-analyzer-deadcode.DeadStores,-warnings-as-errors]
                            px += 100;
                            ^
    
    /home/runner/work/teeworlds/teeworlds/src/game/client/components/stats.cpp:543:4: error: Value stored to 'px' is never read [clang-analyzer-deadcode.DeadStores,-warnings-as-errors]
                            px += 100;
                            ^
    
    /home/runner/work/teeworlds/teeworlds/src/engine/client/textrender.cpp:1062:2: error: Value stored to 'Size' is never read [clang-analyzer-deadcode.DeadStores,-warnings-as-errors]
            Size = PixelSize / ScreenScale.y;
            ^

--- src/engine/client/textrender.cpp
+++ src/engine/client/textrender.cpp
@@ -1033,10 +1033,6 @@ void CTextRender::TextNewline(CTextCursor *pCursor)
 	}
 
 	vec2 ScreenScale = vec2(ScreenWidth/(ScreenX1-ScreenX0), ScreenHeight/(ScreenY1-ScreenY0));
-	float Size = pCursor->m_FontSize;
-	int PixelSize = (int)(Size * ScreenScale.y);
-	Size = PixelSize / ScreenScale.y;
-
 	pCursor->m_LineCount++;
 	pCursor->m_Advance.y = pCursor->m_LineSpacing + pCursor->m_NextLineAdvanceY;
 	pCursor->m_Advance.x = 0;
