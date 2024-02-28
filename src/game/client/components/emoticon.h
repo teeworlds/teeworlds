6701dc1a02788e995a6d76fbc898160b7f075826
ChillerDragon <ChillerDragon@gmail.com>
2024-02-28 13:33:04 +0800

    Remove unused CEmoticon::OnMessage()

--- src/game/client/components/emoticon.h
+++ src/game/client/components/emoticon.h
@@ -25,7 +25,6 @@ public:
 	virtual void OnConsoleInit();
 	virtual void OnRender();
 	virtual void OnRelease();
-	virtual void OnMessage(int MsgType, void *pRawMsg);
 	virtual bool OnCursorMove(float x, float y, int CursorType);
 
 	void Emote(int Emoticon);
